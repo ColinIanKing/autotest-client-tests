79f23bb9bdd93c14f7552375812e09f5347126db
Steve Conklin <sconklin@canonical.com>
2012-09-28 13:28:22 -0500

    UBUNTU: SAUCE: [wakeup_events] Add test

---
+++ wakeup_events/wakeup_events.py
@@ -0,0 +1,45 @@
+import os
+from autotest.client import test, utils
+
+import sys
+
+class wakeup_events(test.test):
+    version = 1
+
+    def initialize(self):
+        self.job.require_gcc()
+
+    def setup(self, instrument_lib_tarball = 'eventstat.tar.bz2'):
+        instrument_lib_tarball = utils.unmap_url(self.bindir, instrument_lib_tarball, self.tmpdir)
+        utils.extract_tarball_to_dir(instrument_lib_tarball, self.srcdir)
+
+	#
+	# make the tools
+	#
+	tool = os.path.join(self.srcdir, 'eventstat')
+	os.chdir(self.srcdir)
+	utils.make()
+	
+    def run_once(self, test_name):
+        if test_name == 'setup':
+            return
+
+        os.chdir(self.srcdir)
+	#
+	#  Meter and tools configuration
+	#
+	os.putenv('DURATION', '600')
+	os.putenv('EVENTSTAT', os.path.join(self.srcdir, 'eventstat'))
+
+	script = os.path.join(os.path.join(self.bindir, 'wakeup_events_tests'), test_name)
+        output = utils.system_output(script, retain_output=True)
+
+        keylist = {}
+        for line in output.splitlines():
+            split = line.split('\t')
+            keylist[split[0]] = split[1]
+
+	print sys.stderr, keylist
+        self.write_perf_keyval(keylist)
+
+# vi:set ts=4 sw=4 expandtab:

1a122b364b1126ac8915c0e4b78857c93d1c0b90
Brad Figg <brad.figg@canonical.com>
2012-09-29 20:44:54 -0700

    UBUNTU: SAUCE: [xfstests] Updated due to autotest upgrade.
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ xfstests/generate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#
+# Read the test definitions and output text which can be included in control.ubuntu. 
+# This will define which tests are valid on different target file systems.
+# This only needs to be done after updating to a new upstream xfstests tarball.
+#
+
+import subprocess
+
+for tn in range(1,276):
+    tns = '%03d' % tn
+    p = subprocess.Popen(['grep', '_supported_fs', 'xfstests/'+tns ], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    #print ('%s: %s' % (tns, out))
+    fstypes = out.split()[1:]
+    if len(fstypes) == 0:
+        fstypes = "['generic']"
+    print "\t    '" + tns + "' : %s," % fstypes

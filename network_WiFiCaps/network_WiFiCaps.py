4fe92ce027a7f0b3a1d1e4131c6e8fcb7305f139
Brad Figg <brad.figg@canonical.com>
2013-04-05 15:09:31 -0700

    UBUNTU: SAUCE: [network_WiFiCaps] wireless capabilities testing
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ network_WiFiCaps/network_WiFiCaps.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging, os, re, string
+
+from autotest.client import test, utils
+from autotest.client.shared import error
+from autotest.client.ubuntu.wifi import WiFi
+
+class network_WiFiCaps(test.test):
+    version = 1
+
+    def setup(self):
+        self.job.setup_dep(['iwcap'])
+        # create a empty srcdir to prevent the error that checks .version
+        if not os.path.exists(self.srcdir):
+            os.mkdir(self.srcdir)
+
+
+    def __parse_iwcap(self, lines):
+        """Parse the iwcap output"""
+
+        results = {}
+        parse_re = re.compile(r'([a-z0-9]*):[ ]*(.*)')
+        for line in lines.split('\n'):
+            line = line.rstrip()
+            logging.info('==> %s' %line)
+            match = parse_re.search(line)
+            if match:
+                results[match.group(1)] = match.group(2)
+                continue
+        return results
+
+
+    def __run_iwcap(self, phy, caps):
+        dir = os.path.join(self.autodir, 'deps', 'iwcap', 'iwcap')
+        print(dir + ' ' + phy + ' ' + string.join(caps))
+        iwcap = utils.run(dir + ' ' + phy + ' ' + string.join(caps))
+        return self.__parse_iwcap(iwcap.stdout)
+
+    def run_once(self):
+        wifi = WiFi()
+        if not wifi.exists():
+            raise error.TestError('No WiFi pci device was found')
+
+        phy = utils.system_output("iw list | awk '/^Wiphy/ {print $2}'")
+        if not phy or 'phy' not in phy:
+            raise error.TestError('WiFi Physical interface not found')
+
+        requiredCaps = wifi.caps
+
+        dep = 'iwcap'
+        dep_dir = os.path.join(self.autodir, 'deps', dep)
+        self.job.install_pkg(dep, 'dep', dep_dir)
+
+        results = self.__run_iwcap(phy, requiredCaps.keys())
+        for cap in requiredCaps:
+            if not cap in results:
+                raise error.TestError('Internal error, ' +
+                    'capability "%s" not handled' % cap)
+            if results[cap] != requiredCaps[cap]:
+                raise error.TestError('Requirement not met: ' +
+                    'cap "%s" is "%s" but expected "%s"'
+                    % (cap, results[cap], requiredCaps[cap]))

81ab0e3f8119929c1b8b2fe3a49b52dc043fc177
Brad Figg <brad.figg@canonical.com>
2014-02-14 08:31:15 -0800

    UBUNTU: SAUCE: [ubuntu_qrt_kernel] Run the tests as the 'ubuntu' user
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ubuntu_qrt_kernel/ubuntu_qrt_kernel.py
+++ ubuntu_qrt_kernel/ubuntu_qrt_kernel.py
@@ -12,6 +12,13 @@ class ubuntu_qrt_kernel(test.test):
         utils.extract_tarball_to_dir(tarball, self.srcdir)
         print(utils.system_output('head %s/scripts/bzr.log' % self.srcdir, retain_output=True))
 
+        # For these tests we need to run as a non-root user. Part of the test is
+        # compiling some test code and so the non-root user needs to be able to
+        # create files in the tmp directory space.
+        #
+        self.results = utils.system_output('find %s -type d | xargs chmod 777' % self.srcdir, retain_output=True)
+
+
     def run_once(self, test_name):
         scripts = os.path.join(self.srcdir, 'scripts')
         os.chdir(scripts)
@@ -19,7 +26,7 @@ class ubuntu_qrt_kernel(test.test):
         if test_name == 'setup':
             return
 
-        cmd = 'python ./%s -v' % test_name
+        cmd = "sudo -u ubuntu python ./%s -v" % test_name
         self.results = utils.system_output(cmd, retain_output=True)
 
 

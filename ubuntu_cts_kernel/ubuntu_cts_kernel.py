fc046e13cd0d44a5645a7c0b2273ad51bfcba9d7
Brad Figg <brad.figg@canonical.com>
2014-06-20 08:21:09 -0700

    [ubuntu_cts_kernel] Moved the looping over the individual tests to the control file for better test granularity.
    It will be easier in the reports to see which of the individual tests
    were run and which passed/failed.
    
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ubuntu_cts_kernel/ubuntu_cts_kernel.py
+++ ubuntu_cts_kernel/ubuntu_cts_kernel.py
@@ -1,7 +1,6 @@
 from autotest.client                        import test,utils
 from autotest.client.shared                 import error, software_manager
 
-BUGS=['lp1256988','lp1153769','lp1262692','lp1026116']
 DEPENDENCIES=['openvswitch-switch', 'iproute2', 'linux-tools-`uname -r`', 'coreutils', 'apparmor']
 
 class ubuntu_cts_kernel(test.test):
@@ -10,10 +9,9 @@ class ubuntu_cts_kernel(test.test):
     def initialize(self):
         pass
 
-    def run_once(self, exit_on_error=True, set_time=True):
-        for bug in BUGS:
-            print('*** %s ***' % bug)
-            cmd = '%s/bugs/%s' % (self.bindir, bug)
-            self.results = utils.system_output(cmd, retain_output=True)
+    def run_once(self, bug, exit_on_error=True, set_time=True):
+        print('*** %s ***' % bug)
+        cmd = '%s/bugs/%s' % (self.bindir, bug)
+        self.results = utils.system_output(cmd, retain_output=True)
 
 # vi:set ts=4 sw=4 expandtab syntax=python:

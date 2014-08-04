e46be6a6659ffccc441e97f6874bc6628c6f2b31
Chris J Arges <chris.j.arges@canonical.com>
2014-08-04 15:04:52 -0500

    UBUNTU: SAUCE: [ubuntu_kvm_unit_tests] patch unittests.cfg to select testcases
    Now you can place a unittests.patch into the directory to patch
    the command used by run_tests.sh. In addition the logic for running
    the tests was simplifed to just run as one monolithic test.
    
    Signed-off-by: Chris J Arges <chris.j.arges@canonical.com>

--- ubuntu_kvm_unit_tests/ubuntu_kvm_unit_tests.py
+++ ubuntu_kvm_unit_tests/ubuntu_kvm_unit_tests.py
@@ -15,20 +15,11 @@ class ubuntu_kvm_unit_tests(test.test):
         utils.configure() 
         utils.make()
 
-        # HACK: enumerate tests that need to be run from run_tests.sh
-        cmd="sed 's/eval $cmdline.*$//g' run_tests.sh > show_tests.sh;\
-             chmod +x show_tests.sh;\
-             ./show_tests.sh -v | egrep '(^./)' > tests.txt"
-        utils.system(cmd)
+        # patch x86/unittests.cfg
+        utils.system('patch -p1 < %s/unittests.patch' % self.bindir)
 
     def run_once(self):
         os.chdir(self.srcdir)
-
-        for test in open('tests.txt').readlines():
-            test_ = test.replace('\n','')
-            # kvm-unit-tests return -le 1 on PASS
-            test_cmd = ('%s; [ $? -le 1 ]' % test_)
-            print ('*** %s ***' % test_)
-            self.results = utils.system_output(test_cmd, retain_output=True, timeout=60)
+        self.results = utils.system_output('./run_tests.sh', retain_output=True, timeout=240)
 
 # vi:set ts=4 sw=4 expandtab:

264bec52267ba8f8a4fc4e6915c9d1492e26e35e
Colin Ian King <colin.king@canonical.com>
2015-05-21 17:02:56 +0100

    UBUNTU: SAUCE: [ubuntu_zfs] Add zfs-tests to autotest-client-tests
    This commit contains the autotest wrapper for the upstream zfs-tests.
    zfs-tests contained in this patch is a snapshot from:
    
       https://github.com/zfsonlinux/zfs-test.git
       at commit 757261871821ec23eaf69f9fc41971f4981b7cc3
         ("Update the README.md to explain autogen.sh and configure.")
    
    The wrapper pulls in the -stable zfs Ubuntu packages from:
        ppa:zfs-native/stable
    
    ..which is based on the zfs-on-linux tarballs from:
        http://zfsonlinux.org/
    
    These tests run to completion, but currently contain a bunch of
    failures and killed tests that timeout which need some attention
    (to be done later).
    
    The output at the end of the test will be a summary of test results
    directly from the zfs-tests on the form:
    
    	Results Summary
    	FAIL     147
    	SKIP     307
    	KILLED    13
    	PASS     223
    
    For now, we need to monitor these stats to see how they vary from
    kernel to kernel.  Hopefully once I have determined why we see
    so many failures and killed tests we can get this into a better state.
    The tests contain a 600 second killer that terminates tests if they
    are deemed to overrun (showing as "KILLED" tests results).
    
    The zfs-tests run on all Ubuntu kernels from 2.6.32 through to 4.0.0.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ ubuntu_zfs/ubuntu_zfs.py
@@ -0,0 +1,66 @@
+#
+#
+import os
+import glob
+from autotest.client                        import test, utils
+import multiprocessing
+
+class ubuntu_zfs(test.test):
+    version = 2
+
+    def initialize(self):
+        self.job.require_gcc()
+
+
+    #
+    # if you change setup, be sure to increment version
+    #
+    def setup(self):
+	utils.system_output('rm /etc/*/S99autotest || true', retain_output=True)
+
+	utils.system_output('add-apt-repository ppa:zfs-native/stable -y', retain_output=True)
+	utils.system_output('apt-get update', retain_output=True)
+	
+	pkgs = ['build-essential', 'gdb', 'git', 'ksh', 'autoconf', 'build-essential',
+		'ubuntu-zfs', 'acl', 'dump', 'kpartx', 'pax',
+		'nfs-kernel-server' ]
+	for pkg in pkgs:
+		print "Installing package " + pkg
+		utils.system_output('apt-get install ' + pkg + ' --yes --force-yes', retain_output=True)
+
+	utils.system_output('useradd zfs-tests || true', retain_output=True)
+	utils.system_output('echo \"zfs-tests    ALL=(ALL)NOPASSWD: ALL\" >> /etc/sudoers', retain_output=True)
+	print "Extracting zfs-test tarball.."
+	tarball = utils.unmap_url(self.bindir, 'zfs-tests.tar.bz2', self.tmpdir)
+	utils.extract_tarball_to_dir(tarball, self.srcdir)
+
+
+	os.chdir(self.srcdir)
+	print "Patching zfs-test tarball.."
+	utils.system('patch -p1 < %s/zfs-tweaks.patch' % self.bindir)
+	print "Building zfs-test tarball.."
+        utils.system('./autogen.sh')
+        utils.configure('')
+        utils.system('SRCDIR=%s make' % self.srcdir)
+        #utils.make()
+	utils.system('modprobe zfs')
+	utils.system('cp %s/test/zfs-tests/runfiles/linux.run %s/linux.run' % (self.srcdir, self.srcdir))
+	
+
+    def run_once(self, test_name):
+	#
+	#  We need to call setup first to trigger setup() being
+	#  invoked, then we can run run_once per test
+	#
+	if test_name == 'setup':
+		return
+
+	os.chdir(self.srcdir)
+        cmd = 'RUNFILE="-c %s/linux.run" make test' % self.srcdir
+        #cmd = 'LINUX=linux make test'
+	print "Running: " + cmd
+        self.results = utils.system_output(cmd, retain_output=True)
+        print self.results
+	print "Done!"
+
+# vi:set ts=4 sw=4 expandtab syntax=python:

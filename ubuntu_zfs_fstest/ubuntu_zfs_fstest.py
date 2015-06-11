a422345560a7faa868128d674ca0d1e19b486a24
Colin Ian King <colin.king@canonical.com>
2015-06-11 18:35:19 +0100

    UBUNTU: SAUCE: [ubuntu_zfs_fstest] Add POSIX zfs fstests
    This test suite checks for POSIX correctness using the
    zfs modified fstest suite.  The test suite tarball in this
    test is from:
    
        https://github.com/zfsonlinux/fstest.git
    
    The test creates a ZFS pool in /pool and a ZFS filesystem as
    /pool/test and runs the POSIX tests against this.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ ubuntu_zfs_fstest/ubuntu_zfs_fstest.py
@@ -0,0 +1,65 @@
+#
+#
+import os
+import glob
+from autotest.client                        import test, utils
+import multiprocessing
+
+class ubuntu_zfs_fstest(test.test):
+    version = 1
+
+    def initialize(self):
+        self.job.require_gcc()
+
+
+    #
+    # if you change setup, be sure to increment version
+    #
+    def setup(self):
+	utils.system_output('rm /etc/*/S99autotest || true', retain_output=True)
+
+	utils.system_output('add-apt-repository ppa:zfs-native/stable -y', retain_output=True)
+	utils.system_output('apt-get update', retain_output=True)
+	
+	pkgs = ['perl', 'build-essential', 'gdb', 'git', 'ksh', 'autoconf', 'build-essential',
+		'ubuntu-zfs', 'acl', 'dump', 'kpartx', 'pax',
+		'nfs-kernel-server' ]
+	for pkg in pkgs:
+		print "Installing package " + pkg
+		utils.system_output('apt-get install ' + pkg + ' --yes --force-yes', retain_output=True)
+
+	print "Extracting fstest tarball.."
+	tarball = utils.unmap_url(self.bindir, 'fstest.tar.bz2', self.tmpdir)
+	utils.extract_tarball_to_dir(tarball, self.srcdir)
+
+	os.chdir(self.srcdir)
+	print "Building fstest.."
+        utils.system('make')
+	print "Loading zfs driver.."
+	utils.system('modprobe zfs')
+
+    def run_once(self, test_name):
+	#
+	#  We need to call setup first to trigger setup() being
+	#  invoked, then we can run run_once per test
+	#
+	if test_name == 'setup':
+		return
+
+	os.chdir(self.srcdir)
+	utils.system('truncate -s 128M /tmp/pool.img')
+	utils.system('zpool create pool /tmp/pool.img')
+	utils.system('zfs create pool/test')
+	os.chdir('/pool/test')
+	
+        cmd = 'prove --nocolor -q -r %s' % self.srcdir
+	print "Running: " + cmd
+        self.results = utils.system_output(cmd, retain_output=True)
+        print self.results
+	os.chdir(self.srcdir)
+	utils.system('zfs destroy pool/test')
+	utils.system('zpool destroy pool')
+	utils.system('rm /tmp/pool.img')
+	print "Done!"
+
+# vi:set ts=4 sw=4 expandtab syntax=python:

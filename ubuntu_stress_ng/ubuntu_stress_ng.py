9374eef0cbd8fd399f32179fe94940ea6a1f7bd8
Colin Ian King <colin.king@canonical.com>
2015-07-17 17:04:04 +0100

    UBUNTU: SAUCE: [ubuntu_stress_ng] Output yaml logs
    stress-ng now supports data gathering on total system loads,
    per process metrics, perf performance stats and also thermal
    zone information.  This change gathers these stats and saves
    results in a YAML output file for later data processing if
    required (such as spotting performance regressions).
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- ubuntu_stress_ng/ubuntu_stress_ng.py
+++ ubuntu_stress_ng/ubuntu_stress_ng.py
@@ -3,6 +3,8 @@
 import os
 from autotest.client                        import test, utils
 import multiprocessing
+import time
+
 
 class ubuntu_stress_ng(test.test):
     version = 1
@@ -30,9 +32,15 @@ class ubuntu_stress_ng(test.test):
     def run_once(self, test_name):
         os.chdir(os.path.join(self.srcdir, 'stress-ng'))
 
+        date_start = time.strftime("%Y-%m-%d")
+        time_start = time.strftime("%H%M")
+        name = os.uname()
+        yaml ='stress-ng_' + name[1] + '_' + name[2] + '_' + date_start + '_' + time_start + '.yaml'
+        yaml = os.path.join(self.resultsdir, yaml)
+
         # Run each stressor for 60 seconds and gather some stats at the end
         #
-        cmd = './stress-ng -v --verbose --timeout 60s --seq 0 --metrics --times'
+        cmd = './stress-ng --verbose --timeout 60s --seq 0 --metrics --times --perf --tz --yaml ' + yaml
         self.results = utils.system_output(cmd, retain_output=True)
 
 # vi:set ts=4 sw=4 expandtab syntax=python:

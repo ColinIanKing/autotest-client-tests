759e3df92b374094b810039380d4cf97bca73369
Colin Ian King <colin.king@canonical.com>
2016-04-08 09:09:20 +0100

    UBUNTU: SAUCE: Add new aufs kernel smoke test
    Functional kernel smoke tests for aufs.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ ubuntu_aufs_smoke_test/ubuntu_aufs_smoke_test.py
@@ -0,0 +1,23 @@
+#
+#
+import os
+from autotest.client                        import test, utils
+import multiprocessing
+
+class ubuntu_aufs_smoke_test(test.test):
+    version = 1
+
+    def initialize(self):
+	pass
+
+    def setup(self):
+	pass
+
+    def run_once(self, test_name):
+        stress_ng = os.path.join(self.srcdir, 'stress-ng', 'stress-ng')
+        cmd = '%s/ubuntu_aufs_smoke_test.sh' % (self.bindir)
+        self.results = utils.system_output(cmd, retain_output=True)
+
+        print self.results
+
+# vi:set ts=4 sw=4 expandtab syntax=python:

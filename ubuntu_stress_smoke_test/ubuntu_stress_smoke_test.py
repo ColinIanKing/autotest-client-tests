3436875b73755f6a5b176117b59b5180925946da
Colin Ian King <colin.king@canonical.com>
2016-04-14 14:07:11 +0100

    UBUNTU: SAUCE: ubuntu_stress_smoke_test: build from stress git repo
    Build stress-ng from upstream git repo so we get the latest and greatest
    version.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ubuntu_stress_smoke_test/ubuntu_stress_smoke_test.py
+++ ubuntu_stress_smoke_test/ubuntu_stress_smoke_test.py
@@ -8,10 +8,19 @@ class ubuntu_stress_smoke_test(test.test):
     version = 1
 
     def initialize(self):
-	pass
+        self.job.require_gcc()
 
     def setup(self):
-	utils.system_output('apt-get install stress-ng --yes --force-yes', retain_output=True)
+        utils.system_output('apt-get update', retain_output=True)
+        cmd = 'apt-get install zlib1g-dev libbsd-dev libattr1-dev ' \
+              'libkeyutils-dev libapparmor-dev apparmor libaio-dev ' \
+              '--yes --allow-downgrades --allow-change-held-packages'
+        utils.system_output(cmd, retain_output=True)
+        os.chdir(self.srcdir)
+        cmd = 'git clone git://kernel.ubuntu.com/cking/stress-ng'
+        self.results = utils.system_output(cmd, retain_output=True)
+        os.chdir(os.path.join(self.srcdir, 'stress-ng'))
+        self.results = utils.system_output('make', retain_output=True)
 
     def run_once(self, test_name):
         cmd = '%s/ubuntu_stress_smoke_test.sh' % (self.bindir)

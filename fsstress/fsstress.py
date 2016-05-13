6be9414c7f495079bf8ecc25442f171e79f7cdeb
Colin Ian King <colin.king@canonical.com>
2016-05-13 19:26:55 +0100

    UBUNTU SAUCE: fsstress: re-work test to stress a range of file systems
    The original test only ran this on the default installed file system,
    I've modified this to run on a loop-backed device with ext2, ext3, ext4,
    xfs and jfs to see what shakes out. This test will run for ~45 minutes
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- fsstress/fsstress.py
+++ fsstress/fsstress.py
@@ -13,15 +13,45 @@ class fsstress(test.test):
         self.tarball = utils.unmap_url(self.bindir, tarball, self.tmpdir)
         utils.extract_tarball_to_dir(self.tarball, self.srcdir)
 
+	utils.system_output('apt-get install xfsprogs jfsutils --assume-yes', retain_output=True)
+
         os.chdir(self.srcdir)
         utils.system('patch -p1 < %s/fsstress-ltp.patch' % self.bindir)
         utils.system('patch -p1 < %s/makefile.patch' % self.bindir)
         utils.make('fsstress')
 
-    def run_once(self, testdir=None, extra_args='', nproc='1000', nops='1000'):
+    def run_once(self, fs, testdir=None, extra_args='', nproc='1000', nops='1000'):
         if not testdir:
             testdir = self.tmpdir
+        image = os.path.join(testdir, 'fs.img')
+        mntpath = os.path.join(testdir, 'mnt')
+	tmppath = os.path.join(mntpath, 'tmp')
+        os.mkdir(mntpath)
 
-        args = '-d %s -p %s -n %s %s' % (testdir, nproc, nops, extra_args)
+        self.results = utils.system_output('truncate --size 1G ' + image, retain_output=True)
+        if fs == 'xfs':
+            self.results += utils.system_output('mkfs.xfs -f ' + image, retain_output=True)
+            self.results += utils.system_output('mount -t xfs -o loop ' + image + ' ' + mntpath, retain_output=True)
+        elif fs == 'ext2':
+            self.results += utils.system_output('mkfs.ext2 ' + image, retain_output=True)
+            self.results += utils.system_output('mount -t ext4 -o loop ' + image + ' ' + mntpath, retain_output=True)
+        elif fs == 'ext3':
+            self.results += utils.system_output('mkfs.ext3 ' + image, retain_output=True)
+            self.results += utils.system_output('mount -t ext4 -o loop ' + image + ' ' + mntpath, retain_output=True)
+        elif fs == 'ext4':
+            self.results += utils.system_output('mkfs.ext4 ' + image, retain_output=True)
+            self.results += utils.system_output('mount -t ext4 -o loop ' + image + ' ' + mntpath, retain_output=True)
+        elif fs == 'jfs':
+            self.results += utils.system_output('mkfs.jfs -f ' + image, retain_output=True)
+            self.results += utils.system_output('mount -t jfs -o loop ' + image + ' ' + mntpath, retain_output=True)
+
+        os.mkdir(tmppath)
+	os.chdir(tmppath)
+        args = '-d %s -p %s -n %s %s' % (tmppath, nproc, nops, extra_args)
         cmd = self.srcdir + '/fsstress ' + args
-        utils.system(cmd)
+        utils.system_output(cmd, retain_output=True)
+	os.chdir(self.tmpdir)
+        self.results += utils.system_output('umount ' + mntpath, retain_output=True)
+	print self.results
+        os.rmdir(mntpath)
+        os.remove(image)

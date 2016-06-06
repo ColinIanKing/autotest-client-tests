1eeb4c220b463d1082e0d3ab17db699fdb42ba7e
Brad Figg <brad.figg@canonical.com>
2016-06-06 13:07:26 -0700

    dma_memtest: Adding required packages installation code
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- dma_memtest/dma_memtest.py
+++ dma_memtest/dma_memtest.py
@@ -4,6 +4,7 @@ import re
 import subprocess
 import shutil
 import logging
+import platform
 from autotest.client import utils, test
 from autotest.client.shared import error
 
@@ -20,7 +21,21 @@ class dma_memtest(test.test):
     """
     version = 1
 
+    def install_required_pkgs(self):
+        arch   = platform.processor()
+        series = platform.dist()[2]
+
+        pkgs = [
+            'build-essential',
+        ]
+        gcc = 'gcc' if arch in ['ppc64le', 'aarch64'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        cmd = 'apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
     def initialize(self):
+        self.install_required_pkgs()
         self.cachedir = os.path.join(os.path.dirname(self.srcdir), 'cache')
         self.nfail = 0
 

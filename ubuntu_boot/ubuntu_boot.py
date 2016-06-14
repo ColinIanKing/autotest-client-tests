f5942dbaf9f06d7a160c747e74fa49c019365715
Brad Figg <brad.figg@canonical.com>
2016-06-14 06:24:56 -0700

    A very simple test to be used for 'boot' testing
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ ubuntu_boot/ubuntu_boot.py
@@ -0,0 +1,12 @@
+import os
+from autotest.client import test, utils
+
+
+class ubuntu_32_on_64(test.test):
+    version = 1
+
+    def run_once(self, test_time=10, exit_on_error=True, set_time=True):
+        cmd = "uname -a"
+        utils.system(cmd)
+        cmd = "lsb_release"
+        utils.system(cmd)

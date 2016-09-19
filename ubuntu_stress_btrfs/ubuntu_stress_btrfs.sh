31faf9db6bc17be6ba01ddc669821e2f637655e5
Colin Ian King <colin.king@canonical.com>
2016-09-19 18:55:11 +0100

    UBUNTU: SAUCE: ubuntu_stress_btrfs: remove single stressors, speeds up test
    The single stressors can be removed, they make the test run for > 4 hours on
    a slow device and the core functionality is in the more complex "hammer it to
    death" test scenario.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- ubuntu_stress_btrfs/ubuntu_stress_btrfs.sh
+++ ubuntu_stress_btrfs/ubuntu_stress_btrfs.sh
@@ -210,17 +210,22 @@ do
 	#
 	#  Single stressors..
 	#
-	do_test $INFO $IONICE $SCHED -t $DURATION --dentry $N --dentries 100000
-	do_test $INFO $IONICE $SCHED -t $DURATION --dir $N
-	do_test $INFO $IONICE $SCHED -t $DURATION --fallocate $N  --fallocate-bytes 256M
-	do_test $INFO $IONICE $SCHED -t $DURATION --flock $N
-	do_test $INFO $IONICE $SCHED -t $DURATION --hdd $N --hdd-opts sync,wr-rnd,rd-rnd,fadv-willneed,fadv-rnd --hdd-bytes 256M --hdd-write-size 512
-	do_test $INFO $IONICE $SCHED -t $DURATION --hdd $N --hdd-opts wr-seq,rd-rnd --hdd-bytes 128M --hdd-write-size 128K
-	do_test $INFO $IONICE $SCHED -t $DURATION --lease $N
-	do_test $INFO $IONICE $SCHED -t $DURATION --link $N  --symlink $N
-	do_test $INFO $IONICE $SCHED -t $DURATION --lockf $N
-	do_test $INFO $IONICE $SCHED -t $DURATION --seek $N --seek-size 256M
-	do_test $INFO $IONICE $SCHED -t $DURATION --utime $N --utime-fsync
+	#  cking: commented these out as they take far too long
+	#  and we test this functionality with the "hammer it to death"
+	#  stressors.  We leave these in just in case we need them
+	#  at some point in the future.
+	#
+	#do_test $INFO $IONICE $SCHED -t $DURATION --dentry $N --dentries 100000
+	#do_test $INFO $IONICE $SCHED -t $DURATION --dir $N
+	#do_test $INFO $IONICE $SCHED -t $DURATION --fallocate $N  --fallocate-bytes 256M
+	#do_test $INFO $IONICE $SCHED -t $DURATION --flock $N
+	#do_test $INFO $IONICE $SCHED -t $DURATION --hdd $N --hdd-opts sync,wr-rnd,rd-rnd,fadv-willneed,fadv-rnd --hdd-bytes 256M --hdd-write-size 512
+	#do_test $INFO $IONICE $SCHED -t $DURATION --hdd $N --hdd-opts wr-seq,rd-rnd --hdd-bytes 128M --hdd-write-size 128K
+	#do_test $INFO $IONICE $SCHED -t $DURATION --lease $N
+	#do_test $INFO $IONICE $SCHED -t $DURATION --link $N  --symlink $N
+	#do_test $INFO $IONICE $SCHED -t $DURATION --lockf $N
+	#do_test $INFO $IONICE $SCHED -t $DURATION --seek $N --seek-size 256M
+	#do_test $INFO $IONICE $SCHED -t $DURATION --utime $N --utime-fsync
 	#
 	#  And hammer it to death..
 	#

86ce61ff93fdfe372db35adf32e8f331e75fc34a
Brad Figg <brad.figg@canonical.com>
2016-10-06 11:17:30 -0700

    ubuntu_boot: the class name was wrong
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ubuntu_boot/ubuntu_boot.py
+++ ubuntu_boot/ubuntu_boot.py
@@ -2,7 +2,7 @@ import os
 from autotest.client import test, utils
 
 
-class ubuntu_32_on_64(test.test):
+class ubuntu_boot(test.test):
     version = 1
 
     def run_once(self, test_time=10, exit_on_error=True, set_time=True):

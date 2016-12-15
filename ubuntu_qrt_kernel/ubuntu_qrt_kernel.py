ColinIanKing/autotest-client-tests

commit c4000b1a0256b601839e8b21a70dd592110ac6d7

*: removed the requied-pkgs scripts and moved their contents into the respective *.py files
Signed-off-by: Brad Figg <brad.figg@canonical.com>

ubuntu_qrt_kernel/ubuntu_qrt_kernel.py
@@ -1,10 +1,25 @@
 import os
+import platform
 from autotest.client import test, utils
 
 class ubuntu_qrt_kernel(test.test):
     version = 1
 
+    def install_required_pkgs(self):
+        arch   = platform.processor()
+        series = platform.dist()[2]
+
+        pkgs = [
+            'build-essential', 'libcap2-bin', 'gawk', 'execstack', 'exim4', 'libcap-dev',
+        ]
+        gcc = 'gcc' if arch in ['ppc64le', 'aarch64'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        cmd = 'apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
     def initialize(self):
+        self.install_rquired_pkgs()
         self.job.require_gcc()
 
     def setup(self, tarball='ubuntu_qrt_kernel.tar.bz2'):

commit 1d177b47f2276024bff3e02d74d0857ad983d0d2

UBUNTU: SAUCE: Fix typo that is causing this test to fail
This test seems to have been broken since May 2016. Fix a typo
in the call self.install_rquired_pkgs -> self.install_required_pkgs

Fixes: c4000b1a0256b6 ("removed the requied-pkgs scripts and moved their contents into the respective *.py files")

Signed-off-by: Colin Ian King <colin.king@canonical.com>

ubuntu_qrt_kernel/ubuntu_qrt_kernel.py
@@ -19,7 +19,7 @@ class ubuntu_qrt_kernel(test.test):
         self.results = utils.system_output(cmd, retain_output=True)
 
     def initialize(self):
-        self.install_rquired_pkgs()
+        self.install_required_pkgs()
         self.job.require_gcc()
 
     def setup(self):
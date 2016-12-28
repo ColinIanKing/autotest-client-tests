f5674f75596fc242eab809be45eb49a5e07ac2ab
Po-Hsu Lin <po-hsu.lin@canonical.com>
2016-12-28 15:54:07 +0800

    rmaptest: add install_required_pkgs for gcc
    gcc is not available for this test on GCE and AWS, add the
    install_required_pkgs function to install it explicitly.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- rmaptest/rmaptest.py
+++ rmaptest/rmaptest.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from autotest.client import test, utils
 
 
@@ -15,7 +16,19 @@ class rmaptest(test.test):
     version = 1
     preserve_srcdir = True
 
+    def install_required_pkgs(self):
+        arch   = platform.processor()
+        series = platform.dist()[2]
+
+        pkgs = []
+        gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        cmd = 'apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
     def initialize(self):
+        self.install_required_pkgs()
         self.job.require_gcc()
 
     def setup(self):

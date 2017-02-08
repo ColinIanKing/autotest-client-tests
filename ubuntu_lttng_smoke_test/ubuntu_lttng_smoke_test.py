e192d6988846d9bdf1151b812078b6711cb29e5b
Colin Ian King <colin.king@canonical.com>
2017-02-08 11:33:29 +0000

    UBUNTU: SAUCE: Add new lttng smoke test
    This is a smoke test of core lttng userspace + kernel DKMS driver
    support.  Tested on Ubuntu 15.10, 16.04, 16.10, 17.04. Will not
    work on earlier releases.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ ubuntu_lttng_smoke_test/ubuntu_lttng_smoke_test.py
@@ -0,0 +1,35 @@
+#
+#
+import platform
+from autotest.client                        import test, utils
+
+class ubuntu_lttng_smoke_test(test.test):
+    version = 99
+
+    def install_required_pkgs(self):
+        arch   = platform.processor()
+        series = platform.dist()[2]
+
+        pkgs = [
+            'build-essential',
+            'babeltrace',
+            'lttng-tools',
+            'lttng-modules-dkms',
+            'liblttng-ust-dev',
+        ]
+        gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        cmd = 'apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
+    def initialize(self):
+        self.install_required_pkgs()
+        self.job.require_gcc()
+
+    def run_once(self, test_name):
+        cmd = '%s/ubuntu_lttng_smoke_test.sh' % (self.bindir)
+        self.results = utils.system_output(cmd, retain_output=True)
+	print self.results
+
+# vi:set ts=4 sw=4 expandtab syntax=python:
---
+++ ubuntu_lttng_smoke_test/ubuntu_lttng_smoke_test.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 Canonical
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+#
+#  Trival "smoke test" lttng tests just to see
+#  if basic functionality works
+#
+SESSION=/tmp/lttng-kernel-trace-$$-session
+SESSION_NAME=test-kernel-session
+TMPFILE=/tmp/lttng-kernel-trace-$$.tmp
+
+passed=0
+failed=0
+
+check()
+{
+	if [ $1 -eq 0 ]; then
+		echo "PASSED ($2)"
+		passed=$((passed + 1))
+		return 0
+	fi
+
+	echo "FAILED ($2)"
+	failed=$((failed + 1))
+	rc=1
+	return 1
+}
+
+test_lttng_session_create()
+{
+	echo "== lttng smoke test of session create/destroy =="
+	lttng create ${SESSION_NAME} --output=${SESSION}
+	check $? "lttng create"
+	lttng destroy
+	check $? "lttng destroy"
+}
+
+test_lttng_list_kernel()
+{
+	echo "== lttng smoke test list kernel events =="
+	lttng list --kernel > $TMPFILE
+	check $? "lttng list --kernel"
+	n=$(wc -l < $TMPFILE | cut -d' ' -f1)
+	if [ $n -lt 3 ]; then
+		echo "FAILED (lttng list --kernel more output expected)"
+	fi
+	rm -f $TMPFILE
+
+	lttng list --kernel --syscall > $TMPFILE
+	check $? "lttng list --kernel --syscall"
+	n=$(wc -l < $TMPFILE | cut -d' ' -f1)
+	if [ $n -lt 3 ]; then
+		echo "FAILED (lttng list --kernel --syscall more output expected)"
+	fi
+	rm -f $TMPFILE
+}
+
+test_lttng_open_close()
+{
+	echo "== lttng smoke test trace open/close system calls =="
+	lttng create ${SESSION_NAME} --output=${SESSION}
+	check $? "lttng create"
+	if [ $? -ne 0 ]; then
+		return 1
+	fi
+
+	lttng enable-event --kernel --syscall open,close
+	check $? "lttng enable-event"
+	if [ $? -ne 0 ]; then
+		lttng destroy
+		return 1
+	fi
+
+	lttng start
+	check $? "lttng start"
+	if [ $? -ne 0 ]; then
+		lttng destroy
+		return 1
+	fi
+
+	cat /proc/cpuinfo > /dev/null
+
+	lttng stop
+	check $? "lttng stop"
+	if [ $? -ne 0 ]; then
+		lttng destroy
+		return 1
+	fi
+
+	lttng destroy
+	check $? "lttng destroy"
+	if [ $? -ne 0 ]; then
+		return 1
+	fi
+
+	babeltrace ${SESSION}* > $TMPFILE
+	opens=$(grep open $TMPFILE | wc -l | cut -d' ' -f1)
+	closes=$(grep close $TMPFILE | wc -l | cut -d' ' -f1)
+	procs=$(grep "/proc/cpuinfo" $TMPFILE | wc -l | cut -d' ' -f1)
+
+	if [ $opens -lt 1 ]; then
+		echo "FAILED (did not trace any open system calls)"
+		rc=1
+		return 1
+	fi
+	if [ $closes -lt 1 ]; then
+		echo "FAILED (did not trace any close system calls)"
+		rc=1
+		return 1
+	fi
+	if [ $procs -lt 1 ]; then
+		echo "FAILED (did not trace open on /proc/cpuinfo)"
+		rc=1
+		return 1
+	fi
+	echo "PASSED (simple system call tracing with babeltrace)"
+	return 0
+}
+
+test_lttng_context_switch()
+{
+	echo "== lttng smoke test trace context switches =="
+	lttng create ${SESSION_NAME} --output=${SESSION}
+	check $? "lttng create"
+	if [ $? -ne 0 ]; then
+		return 1
+	fi
+
+	lttng enable-event --kernel sched_switch
+	check $? "lttng enable-event"
+	if [ $? -ne 0 ]; then
+		lttng destroy
+		return 1
+	fi
+
+	lttng start
+	check $? "lttng start"
+	if [ $? -ne 0 ]; then
+		lttng destroy
+		return 1
+	fi
+
+	sleep 1
+	(dd if=/dev/zero bs=4096 count=8192 | cat | cat | cat | dd bs=4096 > /dev/null) >& /dev/null
+	sleep 1
+
+	lttng stop
+	check $? "lttng stop"
+	if [ $? -ne 0 ]; then
+		lttng destroy
+		return 1
+	fi
+
+	lttng destroy
+	check $? "lttng destroy"
+	if [ $? -ne 0 ]; then
+		return 1
+	fi
+
+	babeltrace ${SESSION}* > $TMPFILE
+	dds=$(grep prev_comm $TMPFILE | grep dd | wc -l | cut -d' ' -f1)
+	cats=$(grep prev_comm $TMPFILE | grep cat | wc -l | cut -d' ' -f1)
+
+	if [ $dds -lt 256 ]; then
+		echo "FAILED (did not trace any dd context switches)"
+		rc=1
+		return 1
+	fi
+	if [ $cats -lt 256 ]; then
+		echo "FAILED (did not trace any cat context switches)"
+		rc=1
+		return 1
+	fi
+	echo "PASSED (simple system call tracing with babeltrace)"
+	return 0
+}
+
+rc=0
+test_lttng_session_create
+echo " "
+test_lttng_list_kernel
+echo " "
+test_lttng_open_close
+echo " "
+test_lttng_context_switch
+echo " "
+
+rm -rf $TMPFILE $SESSION
+
+echo "Summary: $passed passed, $failed failed"
+
+exit $rc

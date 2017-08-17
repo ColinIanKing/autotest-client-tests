ColinIanKing/autotest-client-tests

commit 114e8fb0fc6bce9cde0a902ebc491650896dc414

ubuntu_bpf: variable cleanup
The ubuntu_bpf.py is passing self.bindir to ubuntu_bpf.sh as SRCDIR,
unify the variable naming to make is as BINDIR, and removed the unused
SRCDIR variable.

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_bpf/ubuntu_bpf.py
@@ -35,7 +35,7 @@ class ubuntu_bpf(test.test):
             cmd = 'git clone --depth=1 https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/%s linux' % series
             utils.system(cmd)
 
-        cmd = '%s/ubuntu_bpf.sh %s %s' % (self.bindir, self.srcdir, self.bindir)
+        cmd = '%s/ubuntu_bpf.sh %s %s' % (self.bindir, self.bindir)
         self.results = utils.system_output(cmd, retain_output=True)
 
         print(self.results)

ubuntu_bpf/ubuntu_bpf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #
 # Copyright (C) 2016 Canonical
@@ -21,7 +21,6 @@
 set -e
 rc=0
 BINDIR=$1
-SRCDIR=$2
 ROOT_DIR=`pwd`
 
 # FIXME: This should be done in default adt environment
@@ -49,7 +48,7 @@ git fetch origin master;git reset --hard FETCH_HEAD
 
 git config --local user.email "foo@bar"
 git config --local user.name "Canonical Kernel Testing"
-git am ${SRCDIR}/0001-selftests-just-build-bpf.patch
+git am ${BINDIR}/0001-selftests-just-build-bpf.patch
 
 cd tools/testing/selftests
 make clean

commit 10c66bd052bf227b494006dfd16c41f5acf486fa

ubuntu_bpf: fix string format issue
This bug was introduced in 114e8fb0. I have the variable removed but
not the format string.

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
Signed-off-by: Andy Whitcroft <apw@canonical.com>

ubuntu_bpf/ubuntu_bpf.py
@@ -35,7 +35,7 @@ class ubuntu_bpf(test.test):
             cmd = 'git clone --depth=1 https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/%s linux' % series
             utils.system(cmd)
 
-        cmd = '%s/ubuntu_bpf.sh %s %s' % (self.bindir, self.bindir)
+        cmd = '%s/ubuntu_bpf.sh %s' % (self.bindir, self.bindir)
         self.results = utils.system_output(cmd, retain_output=True)
 
         print(self.results)
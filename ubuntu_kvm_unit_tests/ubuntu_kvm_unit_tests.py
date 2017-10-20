74bd3b42bf104533a6bbd962ee0c1d1e1fbf498a
Po-Hsu Lin <po-hsu.lin@canonical.com>
2017-10-20 15:56:20 +0800

    UBUNTU: SAUCE: ubuntu_kvm_unit_tests: improve the cloud platforms blacklist check
    For non-cloud system, this test will crash with the os.environ['CLOUD'] lookup.
    Fix it by catching the KeyError with try-except.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_kvm_unit_tests/ubuntu_kvm_unit_tests.py
+++ ubuntu_kvm_unit_tests/ubuntu_kvm_unit_tests.py
@@ -10,10 +10,12 @@ class ubuntu_kvm_unit_tests(test.test):
     def install_required_pkgs(self):
         arch   = platform.processor()
         series = platform.dist()[2]
-        cloud  = os.environ['CLOUD']
-
-        if cloud in ['gcp', 'gke', 'aws', 'azure']:
-            raise error.TestError('This test suite does not run correctly on any of these clouds and needs to be investigated.')
+        try:
+            cloud  = os.environ['CLOUD']
+            if cloud in ['gcp', 'gke', 'aws', 'azure']:
+                raise error.TestError('This test suite does not run correctly on any of these clouds and needs to be investigated.')
+        except KeyError:
+            pass
 
         pkgs = [
             'build-essential', 'qemu-kvm',

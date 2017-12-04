4f070fe4f9142c0a8a20f004c90198e1abb59d3c
Brad Figg <brad.figg@canonical.com>
2017-12-04 13:53:30 -0800

    ubuntu_kvm_smoke_test.py: use os.getlogin() instead of the hardcoded 'ubuntu'
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ubuntu_kvm_smoke_test/ubuntu_kvm_smoke_test.py
+++ ubuntu_kvm_smoke_test/ubuntu_kvm_smoke_test.py
@@ -48,7 +48,7 @@ class ubuntu_kvm_smoke_test(test.test):
         elif arch in ['ppc64le']:
             arch = 'ppc64el'
 
-        cmd = 'sudo -u ubuntu %s/the-test %s' % (self.bindir, arch)
+        cmd = 'sudo -u %s %s/the-test %s' % (os.getlogin(), self.bindir, arch)
         self.results = utils.system_output(cmd, retain_output=True)
 
 # vi:set ts=4 sw=4 expandtab syntax=python:

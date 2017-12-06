608f3484011db48f8c88837a410889a1f594d51d
Brad Figg <brad.figg@canonical.com>
2017-12-05 20:53:17 -0800

    ubuntu_kvm_unit_tests: simplified and updated
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ubuntu_kvm_unit_tests/ubuntu_kvm_unit_tests.py
+++ ubuntu_kvm_unit_tests/ubuntu_kvm_unit_tests.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 import platform
 from autotest.client            import test, utils, os_dep
 from autotest.client.shared     import error
@@ -31,10 +32,28 @@ class ubuntu_kvm_unit_tests(test.test):
         self.job.require_gcc()
 
     def setup(self):
+        # Hacky way to use proxy settings, ideally this should be done on deployment stage
+        #
+        proxysets = [
+                {'addr': 'squid.internal', 'desc': 'Running in the Canonical CI environment'},
+                {'addr': '91.189.89.216',  'desc': 'Running in the Canonical enablement environment'},
+                {'addr': '10.245.64.1',    'desc': 'Running in the Canonical enablement environment'}
+            ]
+        for proxy in proxysets:
+            cmd = 'nc -w 2 ' + proxy['addr'] + ' 3128'
+            try:
+                utils.system_output(cmd, retain_output=False)
+                print proxy['desc']
+                os.environ['http_proxy'] = 'http://' + proxy['addr'] + ':3128'
+                os.environ['https_proxy'] = 'https://' + proxy['addr'] + ':3128'
+                break
+            except:
+                pass
+
         arch = platform.processor()
         opt = []
         os.chdir(self.srcdir)
-        cmd = 'git clone --depth=1 git://git.kernel.org/pub/scm/virt/kvm/kvm-unit-tests.git'
+        cmd = 'git clone --depth=1 https://git.kernel.org/pub/scm/virt/kvm/kvm-unit-tests.git'
         self.results = utils.system_output(cmd, retain_output=True)
         os.chdir('kvm-unit-tests')
         if arch == 'ppc64le':
@@ -43,22 +62,23 @@ class ubuntu_kvm_unit_tests(test.test):
         utils.make()
 
         # patch run_tests.sh to build our tests list
-        utils.system('git am %s/runtime_show.patch' % self.bindir)
-        utils.system('./run_tests.sh -v')
+        utils.system('patch -p1 < %s/runtime_show.patch' % self.bindir)
 
     def run_once(self, test_name, cmd=''):
-        os.chdir(self.srcdir)
-        if test_name == 'setup':
-            return
+        os.chdir(self.srcdir + '/kvm-unit-tests')
 
         arch = platform.processor()
         if arch == 'ppc64le':
             # disable smt (simultaneous multithreading) on ppc for kvm
             utils.system('ppc64_cpu --smt=off')
 
-        self.results = utils.system_output(cmd, retain_output=True, timeout=60)
+        output = utils.system_output('./run_tests.sh -v', retain_output=True)
+
         if arch == 'ppc64le':
             # turn smt back on
             utils.system('ppc64_cpu --smt=on')
+
+        if 'unexpected failures' in output:
+            raise error.TestError('Test error, check debug logs for complete test output')
 
 # vi:set ts=4 sw=4 expandtab:

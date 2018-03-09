3896dd2838f1fc2eb34154c841284e256b985fe0
Po-Hsu Lin <po-hsu.lin@canonical.com>
2018-03-09 20:09:39 +0800

    UBUNTU: SAUCE: hwclock: use 2004 Oct 20 for the test
    Change the date for test to the Warty Warthog's birthday.
    Re-order the code for package installation.
    
    This is a workaround for an AMD Naples platform, lodygin,
    which fails to change time to the last century (BIOS issue).
    BugLink: https://bugs.launchpad.net/bugs/1714229
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- hwclock/hwclock.py
+++ hwclock/hwclock.py
@@ -14,10 +14,8 @@ class hwclock(test.test):
         series = platform.dist()[2]
 
         pkgs = [
-            'build-essential',
+            'virt-what',
         ]
-        gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x'] else 'gcc-multilib'
-        pkgs.append(gcc)
 
         cmd = 'apt-get install --yes --force-yes ' + ' '.join(pkgs)
         self.results = utils.system_output(cmd, retain_output=True)
@@ -27,11 +25,10 @@ class hwclock(test.test):
 
     def run_once(self):
         """
-        Set hwclock back to a date in 1980 and verify if the changes took
+        Set hwclock back to a date in 2004 and verify if the changes took
         effect in the system.
         """
         series = platform.dist()[2]
-        utils.system_output('apt-get install virt-what --assume-yes', retain_output=True)
         self.virt = utils.system_output('virt-what', retain_output=True)
         #
         #  Only run if on bare metal, if what is not empty we
@@ -40,14 +37,14 @@ class hwclock(test.test):
         if self.virt != '':
             logging.info('Running inside ' + self.virt + ', not testing ')
         else:
-            logging.info('Setting hwclock to 2/2/80 03:04:00')
-            utils.system_output('/sbin/hwclock --set --date "2/2/80 03:04:00"', retain_output=True)
+            logging.info('Setting hwclock to 2004 Oct. 20 04:10:00')
+            utils.system_output('/sbin/hwclock --set --date "2004/10/20 04:10:00"', retain_output=True)
             date = utils.system_output('LC_ALL=C /sbin/hwclock')
             if series in ['precise', 'trusty', 'vivid', 'xenial']:
-                if not re.match('Sat *Feb *2 *03:04:.. 1980', date):
-                    raise error.TestFail("Failed to set hwclock back to the eighties. Output of hwclock is '%s'" % date)
-            elif not re.match('1980-02-02 03:04:..*', date):
-                raise error.TestFail("Failed to set hwclock back to the eighties. Output of hwclock is '%s'" % date)
+                if not re.match('Wed *Oct *20 *04:10:.. 2004', date):
+                    raise error.TestFail("Failed to set hwclock back to Warthog's birthday. Output of hwclock is '%s'" % date)
+            elif not re.match('2004-10-20 04:10:..*', date):
+                raise error.TestFail("Failed to set hwclock back to Warthog's birthday. Output of hwclock is '%s'" % date)
 
     def cleanup(self):
         """

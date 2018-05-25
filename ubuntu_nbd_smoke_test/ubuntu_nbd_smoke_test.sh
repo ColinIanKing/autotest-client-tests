f883c00612cac360680d88d6233c1dbd4a429547
Colin Ian King <colin.king@canonical.com>
2018-05-25 10:58:55 +0100

    UBUNTU SAUCE: NBD: Add copyright boilerplate
    Add the normal GPL copyright boilerplate and also remove
    3 commented out lines in bash script
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- ubuntu_nbd_smoke_test/ubuntu_nbd_smoke_test.sh
+++ ubuntu_nbd_smoke_test/ubuntu_nbd_smoke_test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-
+#
+# Copyright (C) 2018 Canonical
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
 TESTDIR=nbd-test-$$
 
 MNT=/mnt/$TESTDIR
@@ -8,10 +24,6 @@ NBD_IMAGE_PATH=/tmp/nbd_image.img
 NBD_DEV=/dev/nbd0
 NBD_DUMMY_CONFIG=/tmp/nbd_config_dummy.conf
 NBD_PORT=9999
-
-#echo "STARTING.."
-#set -o posix ; set
-#echo "DONE!"
 
 do_tidy()
 {

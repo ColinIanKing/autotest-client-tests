85761a09f138bea01b86115eb8df7d813dd86afc
Po-Hsu Lin <po-hsu.lin@canonical.com>
2018-07-18 19:30:21 +0800

    UBUNTU SAUCE: ubuntu_bpf: move setup steps to the python script
    Move the test setup steps from the shell script to the python script.
    So that we could group those steps together and make the test itself looks tidy.
    
    The git repo cleanup can be ignored after this, as it will be cloned into the
    src directory, not the result directory.
    
    It also paves the road to split setup task from the test itself in the future.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_bpf/ubuntu_bpf.py
+++ ubuntu_bpf/ubuntu_bpf.py
@@ -9,7 +9,6 @@ class ubuntu_bpf(test.test):
 
     def install_required_pkgs(self):
         arch   = platform.processor()
-        series = platform.dist()[2]
 
         pkgs = [
             'build-essential',
@@ -31,16 +30,44 @@ class ubuntu_bpf(test.test):
     def setup(self):
         self.install_required_pkgs()
         self.job.require_gcc()
-
-    def run_once(self, test_name):
         series = platform.dist()[2]
-        os.chdir(self.srcdir)
+        # Hacky way to use proxy settings, ideally this should be done on deployment stage
+        #
+        proxysets = [
+                {'addr': 'squid.internal', 'desc': 'Running in the Canonical CI environment'},
+                {'addr': '91.189.89.216',  'desc': 'Running in the Canonical enablement environment'},
+                {'addr': '10.245.64.1',    'desc': 'Running in the Canonical enablement environment'}
+            ]
+        for proxy in proxysets:
+            cmd = 'nc -w 2 ' + proxy['addr'] + ' 3128'
+            try:
+                utils.system_output(cmd, retain_output=False)
+                print proxy['desc']
+                os.environ['http_proxy'] = 'http://' + proxy['addr'] + ':3128'
+                os.environ['https_proxy'] = 'http://' + proxy['addr'] + ':3128'
+                break
+            except:
+                pass
 
+        os.chdir(self.srcdir)
         if not os.path.exists('linux'):
             cmd = 'git clone --depth=1 https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/%s linux' % series
             utils.system(cmd)
+        os.chdir(os.path.join(self.srcdir, 'linux'))
+        # Assist local testing by restoring the linux repo to vanilla.
+        cmd = 'git checkout -f;git clean -f -d;git ls-files --others --directory |xargs rm -rvf;rm -rf .git/rebase*'
+        utils.system(cmd)
+        cmd = 'git fetch origin master;git reset --hard FETCH_HEAD'
+        utils.system(cmd)
+        cmd = 'patch -p1 < %s/0001-selftests-just-build-bpf.patch' % self.bindir
+        utils.system(cmd)
+        os.chdir(os.path.join(self.srcdir, 'linux/tools/testing/selftests'))
+        utils.make('clean')
+        utils.make()
 
-        cmd = '%s/ubuntu_bpf.sh %s' % (self.bindir, self.bindir)
+    def run_once(self, test_name):
+        os.chdir(os.path.join(self.srcdir, 'linux/tools/testing/selftests'))
+        cmd = '%s/ubuntu_bpf.sh' % self.bindir
         self.results = utils.system_output(cmd, retain_output=True)
 
         print(self.results)
--- ubuntu_bpf/ubuntu_bpf.sh
+++ ubuntu_bpf/ubuntu_bpf.sh
@@ -20,37 +20,8 @@
 
 set -e
 rc=0
-BINDIR=$1
-ROOT_DIR=`pwd`
-
-# FIXME: This should be done in default adt environment
-# Detect the ubuntu-ci setup
-if echo "" | nc -w 2 squid.internal 3128 >/dev/null 2>&1; then
-    echo "Connecting through squid.internal 3128"
-    export http_proxy="http://squid.internal:3128"
-    export https_proxy="http://squid.internal:3128"
-elif echo "" | nc -w 2 91.189.89.216 3128 >/dev/null 2>&1; then
-    echo "Connecting through 91.189.89.216 3128"
-    export http_proxy="http://91.189.89.216:3128"
-    export https_proxy="http://91.189.89.216:3128"
-elif echo "" | nc -w 2 10.245.64.1 3128 >/dev/null 2>&1; then
-    echo "Connecting through 10.245.64.1 3128"
-    export http_proxy="http://10.245.64.1:3128"
-    export https_proxy="http://10.245.64.1:3128"
-fi
 
 set -x
-cd linux
-
-# Assist local testing by restoring the linux repo to vanilla.
-git checkout -f;git clean -f -d;git ls-files --others --directory |xargs rm -rvf;rm -rf .git/rebase*
-git fetch origin master;git reset --hard FETCH_HEAD
-
-patch -p1 < ${BINDIR}/0001-selftests-just-build-bpf.patch
-
-cd tools/testing/selftests
-make clean
-make
 
 #
 #  Run test_verifier bpf tests
@@ -104,9 +75,4 @@ else
 	echo "FAILED"
 fi
 
-# Cleanup the git repository, we don't want that copied into the testing artifacts.
-#
-cd $ROOT_DIR
-rm -rf linux
 exit $rc
-

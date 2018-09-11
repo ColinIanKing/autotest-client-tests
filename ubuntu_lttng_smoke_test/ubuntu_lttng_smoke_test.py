2e48b876459358e2f1eba8f999c513ddd4fbdc9d
Po-Hsu Lin <po-hsu.lin@canonical.com>
2018-09-11 18:29:29 +0800

    UBUNTU SAUCE: ubuntu_lttng_smoke_test: create swap for some Azure nodes
    BugLink: https://bugs.launchpad.net/bugs/1791032/
    
    Some Azure nodes were allocated with small ram, when compiling the lttng module
    it will fail with error like:
     virtual memory exhausted: Cannot allocate memory
    
    Use a swapfile to solve this, targeting nodes with the following criteria:
      * Running with Azure kernel
      * No swap
      * < 1.8 GB ram
      * > 4 GB free disk space
    
    Tested and passed on node deployed with size BasicA1 / StandardA0 / StandardB1s
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_lttng_smoke_test/ubuntu_lttng_smoke_test.py
+++ ubuntu_lttng_smoke_test/ubuntu_lttng_smoke_test.py
@@ -1,5 +1,6 @@
 #
 #
+import os
 import platform
 from autotest.client                        import test, utils
 from autotest.client.shared                 import error
@@ -28,6 +29,24 @@ class ubuntu_lttng_smoke_test(test.test):
         pass
 
     def setup(self):
+        # Special case for Azure nodes lp: 1791032
+        # Some nodes with small ram will need the swap to build lttng dkms
+        if platform.uname()[2].split('-')[-1] == 'azure':
+            mem_gb = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / (1024.**3)
+            df_gb = os.statvfs('/').f_bsize * os.statvfs('/').f_bavail / (1024.**3)
+            cmd = 'swapon --show'
+            swapon = utils.system_output(cmd)
+            if mem_gb < 1.8 and df_gb > 4.0 and swapon == '':
+                swap_file = '/tmp/swapfile'
+                cmd = 'fallocate -l 2G %s' % swap_file
+                utils.system(cmd)
+                cmd = 'chmod 600 %s' % swap_file
+                utils.system(cmd)
+                cmd = 'mkswap %s' % swap_file
+                utils.system(cmd)
+                cmd = 'swapon %s' % swap_file
+                utils.system(cmd)
+
         self.install_required_pkgs()
         self.job.require_gcc()
         cmd = 'dkms status -m lttng-modules | grep installed'

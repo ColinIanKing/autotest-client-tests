d38b5aafe6cd5fc6eda30e95c346a59055eb5858
Colin Ian King <colin.king@canonical.com>
2018-10-03 17:09:20 +0100

    UBUNTU SAUCE: Add new AWS fio performance based benchmarks
    This adds a new performance benchmark based on a few requested fio
    based benchmarks, namely:
    
    random 4K reads,
    random 4K writes,
    sequential 4K reads,
    sequentiak 4K writes.
    
    File size is set to 2GB for now, this may be to small or too large
    for the final AWS tests, but it's a good placeholder for the moment.
    
    The test extracts the raw bandwidth statistics as well as average
    and standard deviation of I/O operation latenticies.
    
    Typical example output is:
    
    date_ctime "Wed Oct  3 16:22:23 2018"
    date_ns 1538580143906106112
    kernel_version 4.18.0-7-generic
    cpus_online 4
    cpus_total 4
    page_size 4096
    pages_availble 158406
    pages_total 251669
    free_disk_mb 2999.38
    
    read_4k_rand_file_size_mb 128
    read_4k_rand_bandwidth_kb_per_sec 1548.29
    read_4k_rand_latency_usec_average 81.82
    read_4k_rand_latency_stddev 4.76
    
    write_4k_rand_file_size_mb 128
    write_4k_rand_bandwidth_kb_per_sec 872.45
    write_4k_rand_latency_usec_average 145.44
    write_4k_rand_latency_stddev 217.09
    
    read_4k_sequential_file_size_mb 128
    read_4k_sequential_bandwidth_kb_per_sec 1579.01
    read_4k_sequential_latency_usec_average 80.23
    read_4k_sequential_latency_stddev 4.98
    
    write_4k_sequential_file_size_mb 128
    write_4k_sequential_bandwidth_kb_per_sec 1310.72
    write_4k_sequential_latency_usec_average 96.77
    write_4k_sequential_latency_stddev 205.17
    
    .. the above can be extracted from the log in a machine parseable way
    to allow us to produce machine generated graphs if required.
    
    Warning messages are produced if the file size is less than 1/2 of the
    total free memory - this scenario can end up with bogos stats because
    file cache may be skewing the results.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ ubuntu_performance_fio_aws/ubuntu_performance_fio_aws.py
@@ -0,0 +1,171 @@
+#
+#
+import os
+import platform
+import time
+import re
+from autotest.client import test, utils
+
+file_size_mb=2048
+
+class ubuntu_performance_fio_aws(test.test):
+    version = 0
+
+    def install_required_pkgs(self):
+        arch   = platform.processor()
+        series = platform.dist()[2]
+
+        pkgs = [
+            'libaio-dev',
+        ]
+        gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x'] else 'gcc-multilib'
+        pkgs.append(gcc)
+
+        cmd = 'apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
+    def initialize(self):
+        pass
+
+    def setup(self):
+        self.install_required_pkgs()
+        self.job.require_gcc()
+
+        os.chdir(self.srcdir)
+        self.results = utils.system_output('unxz < %s | tar xf - ' % os.path.join(self.bindir, 'fio-3.10.tar.xz'), retain_output=True)
+        os.chdir(os.path.join(self.srcdir, 'fio-3.10'))
+        self.results += utils.system_output('make', retain_output=True)
+
+        print self.results
+
+    def get_filesystem_free_mbytes(self):
+        fd = os.open(self.bindir, os.O_RDONLY)
+        statvfs = os.fstatvfs(fd)
+        os.close(fd);
+        return statvfs.f_bsize * statvfs.f_bavail / (1024.0 * 1024.0)
+
+
+    def get_sysinfo(self):
+        print
+        print 'date_ctime "' + time.ctime() + '"'
+        print 'date_ns %-30.0f' % (time.time() * 1000000000)
+        print 'kernel_version ' + platform.uname()[2]
+        print 'cpus_online ' + utils.system_output('getconf _NPROCESSORS_ONLN', retain_output=True)
+        print 'cpus_total ' + utils.system_output('getconf _NPROCESSORS_CONF', retain_output=True)
+        print 'page_size ' + utils.system_output('getconf PAGE_SIZE', retain_output=True)
+        print 'pages_availble ' + utils.system_output('getconf _AVPHYS_PAGES', retain_output=True)
+        print 'pages_total ' + utils.system_output('getconf _PHYS_PAGES', retain_output=True)
+        print 'free_disk_mb %.2f' % (self.get_filesystem_free_mbytes())
+        print
+
+    def print_stats(self, benchmark, results, fields):
+        return
+
+    def drop_cache(self):
+        #
+        #  Ensure cache won't affect test by dropping caches
+        #  and ensuring data is sync'd
+        #
+        utils.system('sync')
+        f = open("/proc/sys/vm/drop_caches", "w")
+        f.write("3")
+        f.close()
+
+    def run_fio(self, testname):
+        kb_scale = {
+            "KiB":  1024.0 / 1000.0,
+            "KB": 1000.0 / 1000.0,
+            "MiB":  1024.0 * 1024.0 / 1000.0,
+            "MB": 1000.0 * 1000.0 / 1000.0,
+            "GiB":  1024.0 * 1024.0 * 1024.0 / 1000.0,
+            "GB": 1000.0 * 1000.0 * 1000.0 / 1000.0,
+        }
+
+        usec_scale = {
+            "(nsec)": 1.0 / 1000,
+            "(usec)": 1.0,
+            "(msec)": 1000.0,
+            "(sec)" : 1000000.0,
+        }
+
+        #
+        #  Edit various fio configs to use dynamic settings
+        #  relevant to this test location and test size
+        #
+        file = testname + ".fio"
+        fin = open(os.path.join(self.bindir, file), "r")
+        fout = open(os.path.join(self.srcdir, file), "w")
+        file_size = "%dM" % (file_size_mb)
+
+        for line in fin:
+            line = line.replace("DIRECTORY", self.srcdir)
+            line = line.replace("SIZE", file_size)
+            fout.write(line)
+        fin.close()
+        fout.close()
+
+        self.drop_cache()
+
+        #
+        #  Run fio
+        #
+        cmd = os.path.join(self.srcdir, 'fio-3.10', 'fio') + " " + os.path.join(self.srcdir, file)
+        results = utils.system_output(cmd, retain_output=True)
+        bw = 0.0
+        avg = 0.0
+        stdev = 0.0
+
+        #
+        #  Extract pertinent factoids, bandwidth and latencies
+        #
+        for l in results.splitlines():
+            for s in l.split():
+                if s.startswith("BW="):
+                    bw = float(re.findall(r"[-+]?\d*\.*\d+", s)[0])
+                    for sc in kb_scale:
+                        if sc in s:
+                            bw = bw * kb_scale[sc]
+
+            idx_avg = l.find("avg=")
+            idx_stdev = l.find("stdev=")
+
+            if " lat" in l and idx_avg > -1 and idx_stdev > -1:
+                avg = float(re.findall("[-+]?\d*\.*\d+", l[idx_avg:])[0])
+                stdev = float(re.findall("[-+]?\d*\.*\d+", l[idx_stdev:])[0])
+                for sc in usec_scale:
+                    if sc in l:
+                        avg = avg * usec_scale[sc]
+
+        testname = testname.replace("-","_")
+        print
+        print "%s_file_size_mb %s" % (testname, file_size_mb)
+        print "%s_bandwidth_kb_per_sec %.2f" % (testname, bw)
+        print "%s_latency_usec_average %.2f" % (testname, avg)
+        print "%s_latency_stddev %.2f" % (testname, stdev)
+
+    def run_once(self, test_name):
+        if test_name == 'setup':
+            self.setup()
+            self.get_sysinfo()
+            return
+
+        #
+        #  Drop cache to get a good idea of how much free memory can be used
+        #
+        self.drop_cache()
+        page_size = float(utils.system_output('getconf PAGE_SIZE', retain_output=True))
+        pages_available = float(utils.system_output('getconf _AVPHYS_PAGES', retain_output=True))
+        mem_mb = page_size * pages_available / (1024.0 * 1024.0)
+
+        if mem_mb > file_size_mb:
+            print "\nWARNING: file size of %.2f MB should be at least twice the free memory size of %.2f MB" % (file_size_mb, mem_mb)
+
+        free_mb = self.get_filesystem_free_mbytes()
+        if free_mb > file_size_mb:
+            self.run_fio(test_name)
+        else:
+            print 'cannot execute "%s", required %dMB, only got %dMB on disc' % (test_name, file_size_mb, free_mb)
+        print
+
+
+# vi:set ts=4 sw=4 expandtab syntax=python:

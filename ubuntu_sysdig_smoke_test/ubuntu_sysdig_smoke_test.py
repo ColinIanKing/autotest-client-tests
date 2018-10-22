c216bea862dc5d23f156d1529df119c3fb58bde1
Po-Hsu Lin <po-hsu.lin@canonical.com>
2018-10-22 18:13:36 +0800

    UBUNTU SAUCE: ubuntu_sysdig_smoke_test: unload the sysdig module in the end
    BugLink: https://bugs.launchpad.net/bugs/1798295
    
    Just like the lttng test (fixed in commit 8b193fcf), the kernel security test
    will complain about the sysdig_probe module tainting the kernel. This only
    happens on s390x since they will not getre-deployed after each run.
    
    As it will not get loaded again automatically after reboot. Unload the
    sysdig_probe modules can fix this issue.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_sysdig_smoke_test/ubuntu_sysdig_smoke_test.py
+++ ubuntu_sysdig_smoke_test/ubuntu_sysdig_smoke_test.py
@@ -23,6 +23,10 @@ class ubuntu_sysdig_smoke_test(test.test):
     def run_once(self, test_name):
         cmd = '%s/ubuntu_sysdig_smoke_test.sh' % (self.bindir)
         self.results = utils.system_output(cmd, retain_output=True)
-	print self.results
+        print self.results
+
+    def cleanup(self):
+        cmd = 'modprobe -r sysdig_probe'
+        self.results = utils.system_output(cmd, retain_output=False)
 
 # vi:set ts=4 sw=4 expandtab syntax=python:

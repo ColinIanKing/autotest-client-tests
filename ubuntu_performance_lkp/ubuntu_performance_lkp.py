bcfa24685ae71f14ede7cb2f1c2aa4b90f34a247
Colin Ian King <colin.king@canonical.com>
2018-12-13 16:55:21 +0000

    UBUNTU: SAUCE: Add LKP performance tests, vm-scalability benchmarks
    Add in a subset of the vm-scalability benchmarks; ones that work
    well on the target test platform and tests that allow us to get
    some sensible data from the benchmark output.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- ubuntu_performance_lkp/ubuntu_performance_lkp.py
+++ ubuntu_performance_lkp/ubuntu_performance_lkp.py
@@ -11,7 +11,7 @@ import platform
 test_iterations = 3
 
 class ubuntu_performance_lkp(test.test):
-    version = 0
+    version = 1
 
     def is_number(self, s):
         try:
@@ -55,19 +55,10 @@ class ubuntu_performance_lkp(test.test):
             utils.system_output('lkp install jobs/%s || true' % lkp_job, retain_output=True)
             utils.system_output('lkp split jobs/%s || true' % lkp_job, retain_output=True)
 
-    def run_once(self, lkp_job, lkp_jobs):
-        if lkp_job == 'setup':
-            #self.setup(lkp_jobs)
-            return
-
-        test_name = lkp_job
-        test_name = test_name.replace("-", "_")
-        test_pass = True
-        bogoops = []
-
+    def run_aim9(self, lkp_job, lkp_jobs, test_name):
+        values = []
         for i in range(test_iterations):
             print "Testing %s: %d of %d" % (lkp_job, i + 1, test_iterations)
-
             os.chdir(os.path.join(self.srcdir, 'lkp-tests'))
             cmd = 'sudo lkp run %s' % lkp_job
             self.results = utils.system_output(cmd, retain_output=True)
@@ -87,21 +78,67 @@ class ubuntu_performance_lkp(test.test):
                 if len(chunks) > 0 and '---------' in chunks[0]:
                     found_dashes = True;
                 elif found_dashes and len(chunks) > 5 and chunks[0] == '1' and self.is_number(chunks[5]):
-                    bogoops.append(float(chunks[5]))
+                    values.append(float(chunks[5]))
                     found_dashes = False
 
-        minimum = min(bogoops)
-        maximum = max(bogoops)
-        average = sum(bogoops) / len(bogoops)
+        print "%s_bogoops" % (test_name), "%.3f " * len(values) % tuple(values)
+        return values
+
+    def run_vm_scalability(self, lkp_job, lkp_jobs, test_name):
+        values = []
+        for i in range(test_iterations):
+            print "Testing %s: %d of %d" % (lkp_job, i + 1, test_iterations)
+            os.chdir(os.path.join(self.srcdir, 'lkp-tests'))
+            cmd = 'sudo lkp run %s' % lkp_job
+            self.results = utils.system_output(cmd, retain_output=True)
+
+            #
+            # parse text, find KB/s data in fields as follows:
+            #
+            # 53614772232 bytes / 300627955 usecs = 174162 KB/s
+            # 53358395400 bytes / 300632043 usecs = 173327 KB/s
+            # 56508841992 bytes / 300632313 usecs = 183561 KB/s
+            # 56203182088 bytes / 300632709 usecs = 182568 KB/s
+            #
+            for line in self.results.splitlines():
+                chunks = line.split()
+                if len(chunks) >= 8 and chunks[1] == "bytes" and chunks[4] == "usecs" and chunks[7] == "KB/s":
+                    values.append(float(chunks[6]))
+
+        print "%s_kb_per_sec" % (test_name), "%.3f " * len(values) % tuple(values)
+        return values
+
+    def run_once(self, lkp_job, sub_job, lkp_jobs):
+        if lkp_job == 'setup':
+            #self.setup(lkp_jobs)
+            return
+
+        job_funcs = {
+            'aim9.yaml':           self.run_aim9,
+            'vm-scalability.yaml': self.run_vm_scalability,
+        }
+
+        test_name = sub_job
+        test_name = test_name.replace("-", "_")
+        test_pass = True
+
+        print
+        if lkp_job in job_funcs:
+            values = job_funcs[lkp_job](sub_job, lkp_jobs, test_name)
+        else:
+            print "Cannot find running/parser for %s" % lkp_job
+            return
+
+        minimum = min(values)
+        maximum = max(values)
+        average = sum(values) / len(values)
         max_err = (maximum - minimum) / average * 100.0
 
-        if len(bogoops) > 1:
-            stddev = sqrt(float(reduce(lambda x, y: x + y, map(lambda x: (x - average) ** 2, bogoops))) / (len(bogoops) - 1))
+        if len(values) > 1:
+            stddev = sqrt(float(reduce(lambda x, y: x + y, map(lambda x: (x - average) ** 2, values))) / (len(values) - 1))
         else:
             stddev = 0.0
         percent_stddev = (stddev / average) * 100.0
-        print
-        print "%s_bogoops" % (test_name), "%.3f " * len(bogoops) % tuple(bogoops)
         print "%s_minimum %.3f" % (test_name, minimum)
         print "%s_maximum %.3f" % (test_name, maximum)
         print "%s_average %.3f" % (test_name, average)

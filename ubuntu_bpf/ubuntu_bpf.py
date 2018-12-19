9da65989513d87b74b7599b5fb8476432c1b551c
Po-Hsu Lin <po-hsu.lin@canonical.com>
2018-12-19 15:48:58 +0800

    UBUNTU SAUCE: ubuntu_bpf: remove the proxy setting
    As we're just downloading the source package from launchpad,
    the connection should be working without a proxy.
    
    Clean up the proxy setting hack.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_bpf/ubuntu_bpf.py
+++ ubuntu_bpf/ubuntu_bpf.py
@@ -40,24 +40,6 @@ class ubuntu_bpf(test.test):
     def setup(self):
         self.install_required_pkgs()
         self.job.require_gcc()
-        series = platform.dist()[2]
-        # Hacky way to use proxy settings, ideally this should be done on deployment stage
-        #
-        proxysets = [
-                {'addr': 'squid.internal', 'desc': 'Running in the Canonical CI environment'},
-                {'addr': '91.189.89.216',  'desc': 'Running in the Canonical enablement environment'},
-                {'addr': '10.245.64.1',    'desc': 'Running in the Canonical enablement environment'}
-            ]
-        for proxy in proxysets:
-            cmd = 'nc -w 2 ' + proxy['addr'] + ' 3128'
-            try:
-                utils.system_output(cmd, retain_output=False)
-                print proxy['desc']
-                os.environ['http_proxy'] = 'http://' + proxy['addr'] + ':3128'
-                os.environ['https_proxy'] = 'http://' + proxy['addr'] + ':3128'
-                break
-            except:
-                pass
 
         os.chdir(self.srcdir)
         if not os.path.exists('linux'):

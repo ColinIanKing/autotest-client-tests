83fd2d7a3d34157fa22d247abc88dda722d455c3
Po-Hsu Lin <po-hsu.lin@canonical.com>
2019-06-18 19:40:29 +0800

    UBUNTU SAUCE: ubuntu_futex: add proxy setting and change the git source
    Our firewall does not support git protocal, change it to https and add
    the proxy configuration hack.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_futex/ubuntu_futex.py
+++ ubuntu_futex/ubuntu_futex.py
@@ -30,8 +30,26 @@ class ubuntu_futex(test.test):
     def setup(self):
         self.install_required_pkgs()
         self.job.require_gcc()
+        # Hacky way to use proxy settings, ideally this should be done on deployment stage
+        #
+        proxysets = [
+                {'addr': 'squid.internal', 'desc': 'Running in the Canonical CI environment'},
+                {'addr': '91.189.89.216',  'desc': 'Running in the Canonical enablement environment'},
+                {'addr': '10.245.64.1',    'desc': 'Running in the Canonical enablement environment'}
+            ]
+        for proxy in proxysets:
+            cmd = 'nc -w 2 ' + proxy['addr'] + ' 3128'
+            try:
+                utils.system_output(cmd, retain_output=False)
+                print proxy['desc']
+                os.environ['http_proxy'] = 'http://' + proxy['addr'] + ':3128'
+                os.environ['https_proxy'] = 'http://' + proxy['addr'] + ':3128'
+                break
+            except:
+                pass
+
         os.chdir(self.srcdir)
-        cmd = 'git clone --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/dvhart/futextest.git'
+        cmd = 'git clone --depth=1 https://git.kernel.org/pub/scm/linux/kernel/git/dvhart/futextest.git'
         self.results = utils.system_output(cmd, retain_output=True)
 
         os.chdir(os.path.join(self.srcdir, 'futextest', 'functional'))

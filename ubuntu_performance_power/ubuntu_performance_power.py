855e052b6b672d1759c97d7b62d6804facb631f4
Colin Ian King <colin.king@canonical.com>
2019-06-20 13:04:44 +0100

    UBUNTU SAUCE: ubuntu_performance_power: disable services and set rlimits
    Performance tests need to be run with systemd services disabled and
    file rlimits set high to ensure stable results.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- ubuntu_performance_power/ubuntu_performance_power.py
+++ ubuntu_performance_power/ubuntu_performance_power.py
@@ -5,6 +5,8 @@ from autotest.client                        import test, utils
 from math import sqrt
 import platform
 import time
+import subprocess
+import resource
 
 #
 # Number of test iterations to get min/max/average stats
@@ -13,6 +15,53 @@ test_iterations = 5
 
 class ubuntu_performance_power(test.test):
     version = 0
+
+    systemd_services = [
+        "smartd.service",
+        "iscsid.service",
+        "apport.service",
+        "cron.service",
+        "anacron.timer",
+        "apt-daily.timer",
+        "apt-daily-upgrade.timer",
+        "fstrim.timer",
+        "logrotate.timer",
+        "motd-news.timer",
+        "man-db.timer",
+    ]
+    systemctl = "systemctl"
+
+    def stop_services(self):
+        stopped_services = []
+        for service in self.systemd_services:
+            cmd = "%s is-active --quiet %s" % (self.systemctl, service)
+            result = subprocess.Popen(cmd, shell=True)
+            result.communicate()
+            if result.returncode == 0:
+                cmd = "%s stop %s" % (self.systemctl, service)
+                result = subprocess.Popen(cmd, shell=True)
+                result.communicate()
+                if result.returncode == 0:
+                    stopped_services.append(service)
+                else:
+                    print "WARNING: could not stop %s" % (service)
+        return stopped_services
+
+    def start_services(self, services):
+        for service in services:
+            cmd = "%s start %s" % (self.systemctl, service)
+            result = subprocess.Popen(cmd, shell=True)
+            result.communicate()
+            if result.returncode != 0:
+                print "WARNING: could not start %s" % (service)
+
+    def set_rlimit_nofile(self, newres):
+        oldres = resource.getrlimit(resource.RLIMIT_NOFILE)
+        resource.setrlimit(resource.RLIMIT_NOFILE, newres)
+        return oldres
+
+    def restore_rlimit_nofile(self, res):
+        resource.setrlimit(resource.RLIMIT_NOFILE, res)
 
     def install_required_pkgs(self):
         arch   = platform.processor()
@@ -88,6 +137,9 @@ class ubuntu_performance_power(test.test):
         if test_name == 'setup':
             return self.get_sysinfo()
 
+        self.stopped_services = self.stop_services()
+        self.oldres = self.set_rlimit_nofile((500000, 500000))
+
         os.chdir(os.path.join(self.srcdir, 'stress-ng'))
         cmd = "%s/ubuntu_performance_power.sh '%s' '%s' %d %d" % (self.bindir, test_name, options, test_iterations, instances)
         self.results = utils.system_output(cmd, retain_output=True)
@@ -102,5 +154,8 @@ class ubuntu_performance_power(test.test):
         if test_pass:
             print "PASS: test passes specified performance thresholds"
         print
+
+        self.set_rlimit_nofile(self.oldres)
+        self.start_services(self.stopped_services)
 
 # vi:set ts=4 sw=4 expandtab syntax=python:

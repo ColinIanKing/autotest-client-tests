5c215d3e04b9fc99ecbde2a53a823d50d3c0fffc
Colin Ian King <colin.king@canonical.com>
2020-04-30 16:48:09 +0100

    UBUNTU: SAUCE: add zfs encryption performance tests
    Add 6 different ZFS encryption performance tests, each measuring 10
    iterations of write and reads of data in a ram disk.  Putting the data
    in a ramdisk allows us to factor out the noise of SSD or HDD backing
    storage to get a better idea of the encryption overhead.
    
    Output is in a machine parsable format that can be machine scraped and
    dumped into influxdb for graphing, for example:
    
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_wr_duration_seconds_average 6.6886
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_wr_rate_mbytes_per_second_average 78.3800
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_wr_user_time_average .3110
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_wr_kern_time_average 4.7200
    16:39:09 DEBUG| [stdout]
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_rd_duration_seconds_average 7.1901
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_rd_rate_mbytes_per_second_average 72.9200
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_rd_user_time_average .3060
    16:39:09 DEBUG| [stdout] zfs_encryption_aes_192_gcm_rd_kern_time_average 6.8660
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ ubuntu_performance_zfs_encryption/ubuntu_performance_zfs_encryption.py
@@ -0,0 +1,43 @@
+#
+#
+from autotest.client                        import test, utils
+import platform
+
+class ubuntu_performance_zfs_encryption(test.test):
+    version = 3
+
+    def install_required_pkgs(self):
+        arch   = platform.processor()
+        series = platform.dist()[2]
+
+        pkgs = [
+            'xfsprogs', 'git', 'acl', 'libattr1-dev',
+        ]
+
+        if series in ['precise', 'trusty']:
+            utils.system_output('add-apt-repository ppa:zfs-native/stable -y', retain_output=True)
+            utils.system_output('apt-get update || true', retain_output=True)
+            pkgs.append('ubuntu-zfs')
+        elif series == 'wily':
+            pkgs.append('zfs-dkms')
+            pkgs.append('zfsutils-linux')
+        else:
+            pkgs.append('zfsutils-linux')
+
+        cmd = 'apt-get install --yes --force-yes ' + ' '.join(pkgs)
+        self.results = utils.system_output(cmd, retain_output=True)
+
+    def initialize(self):
+        pass
+
+    def setup(self):
+        self.install_required_pkgs()
+        utils.system('modprobe zfs')
+
+    def run_once(self, test_name):
+        if test_name == 'setup':
+            return
+        cmd = '%s/%s %s' % (self.bindir, test_name, self.srcdir)
+        self.results = utils.system_output(cmd, retain_output=True)
+
+# vi:set ts=4 sw=4 expandtab syntax=python:

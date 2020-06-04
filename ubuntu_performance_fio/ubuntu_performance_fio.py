c0a570ed9527bc2203a85487b168af560e675f26
Sean Feole <sean.feole@canonical.com>
2020-06-04 12:29:14 -0400

    UBUNTU SAUCE: ubuntu_performance_fio will not build in 18.04
    Commit #81c37704 Added a patch to fix some build errors in 18.10+
    The patch renamed gettid which resolved a conflict between gettid
    in glibc and fio.  Unfortunately this patch broke building FIO on
    older series like 18.04. The following change simply adds a rule
    to only apply the patch if the host machine != bionic.
    
    Signed-off-by: Sean Feole <sean.feole@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>

--- ubuntu_performance_fio/ubuntu_performance_fio.py
+++ ubuntu_performance_fio/ubuntu_performance_fio.py
@@ -188,7 +188,8 @@ class ubuntu_performance_fio(test.test):
 
         os.chdir(self.srcdir)
         self.results = utils.system_output('unxz < %s | tar xf - ' % os.path.join(self.bindir, 'fio-3.10.tar.xz'), retain_output=True)
-        self.results += utils.system_output('patch -p1 < %s' % os.path.join(self.bindir,'0001-Rename-gettid-to-sys_gettid-to-avoid-name-clash.patch'), retain_output=True)
+        if platform.linux_distribution()[2] not in "bionic":
+            self.results += utils.system_output('patch -p1 < %s' % os.path.join(self.bindir,'0001-Rename-gettid-to-sys_gettid-to-avoid-name-clash.patch'), retain_output=True)
         os.chdir(os.path.join(self.srcdir, 'fio-3.10'))
         self.results += utils.system_output('make', retain_output=True)
 

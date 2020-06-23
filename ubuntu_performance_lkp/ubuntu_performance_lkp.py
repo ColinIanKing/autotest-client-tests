ColinIanKing/autotest-client-tests

commit 1c5cec7366e515974de092ee6ff2867ee97fcd95

UBUNTU: SAUCE: ubuntu_performance_lkp: Conditionalize lkp-tests commit to use
The testing performed on akis needs to use a specific commit in the
lkp-tests tree but other machines should use a newer commit where things
are known to work better on the current Ubuntu development release
(Focal).

The upstream lkp-tests commit selected for the non-akis case includes a
handful of recent fixes for newer Ubuntu releases.

Suggested-by: Colin Ian King <colin.king@canonical.com>
Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
[PHLin: context adjustment base on the changes made in c75afd555474]
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_performance_lkp/ubuntu_performance_lkp.py
@@ -14,7 +14,6 @@ import resource
 # Number of test iterations to get min/max/average stats
 #
 test_iterations = 3
-commit='7078336702a53c99f3a17ad1ca2af9a3323a818c'
 
 class ubuntu_performance_lkp(test.test):
     version = 7
@@ -109,7 +108,7 @@ class ubuntu_performance_lkp(test.test):
             s.close()
         return ipaddr
 
-    def setup(self, lkp_jobs):
+    def setup(self, lkp_jobs, lkp_commit):
         self.install_required_pkgs()
         self.job.require_gcc()
 
@@ -125,7 +124,9 @@ class ubuntu_performance_lkp(test.test):
             self.results += utils.system_output('git clone https://github.com/intel/lkp-tests', retain_output=True)
 
         os.chdir(os.path.join(self.srcdir, 'lkp-tests'))
-        self.results += utils.system_output('git checkout -B stable ' + commit, retain_output=True)
+
+        if lkp_commit:
+            self.results += utils.system_output('git checkout -B stable ' + commit, retain_output=True)
         #
         # New distros use libarchive-tools and not bsdtar so edit the package name
         # https://github.com/intel/lkp-tests/issues/50
@@ -555,10 +556,10 @@ class ubuntu_performance_lkp(test.test):
         print 'pages_available ' + utils.system_output('getconf _AVPHYS_PAGES', retain_output=True)
         print 'pages_total ' + utils.system_output('getconf _PHYS_PAGES', retain_output=True)
 
-    def run_once(self, lkp_job, sub_job, lkp_jobs):
+    def run_once(self, lkp_job, sub_job, lkp_jobs, lkp_commit = None):
         if lkp_job == 'setup':
             self.get_sysinfo()
-            self.setup(lkp_jobs)
+            self.setup(lkp_jobs, lkp_commit)
             return
 
         job_funcs = {

ubuntu_performance_lkp/control
@@ -1,3 +1,5 @@
+import os
+
 AUTHOR = "Ubuntu"
 NAME = "ubuntu_performance_lkp"
 CRITERIA = """
@@ -83,12 +85,18 @@ lkp_jobs = {
           'vm-scalability-300s-mremap-xread-rand-mt.yaml' ],
 }
 
-results = job.run_test_detail(name, tag='setup', lkp_job = 'setup', sub_job = 'setup', lkp_jobs = lkp_jobs)
+if (os.uname()[1] == 'akis') or \
+   ('TEST_CONFIG' in os.environ and 'config' in os.environ['TEST_CONFIG']):
+    lkp_commit = '7078336702a53c99f3a17ad1ca2af9a3323a818c'
+else:
+    lkp_commit = 'ef0e92c3bdb9c2647139a51c8bee0538cf452cb4'
+
+results = job.run_test_detail(name, tag='setup', lkp_job = 'setup', sub_job = 'setup', lkp_jobs = lkp_jobs, lkp_commit = lkp_commit)
 print results
 
 for lkp_job in sorted(lkp_jobs):
     print "JOB '%s'" % lkp_job
     for sub_job in lkp_jobs[lkp_job]:
-        results = job.run_test_detail(name, tag=sub_job, lkp_job = lkp_job, sub_job = sub_job, lkp_jobs = lkp_jobs)
+        results = job.run_test_detail(name, tag=sub_job, lkp_job = lkp_job, sub_job = sub_job, lkp_jobs = lkp_jobs, lkp_commit = lkp_commit)
 
 # vi:set ts=4 sw=4 expandtab syntax=python:

commit 85ac087c0c158dd1d3089ca6d29554890e688b4e

UBUNTU: SAUCE: ubuntu_performance_lkp: fix commit variable name typo
The commit being used is in variable lkp_commit and not commit. Fix this.

Fixes: 1c5cec7366e5 ("UBUNTU: SAUCE: ubuntu_performance_lkp: Conditionalize lkp-tests commit to use")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

ubuntu_performance_lkp/ubuntu_performance_lkp.py
@@ -126,7 +126,7 @@ class ubuntu_performance_lkp(test.test):
         os.chdir(os.path.join(self.srcdir, 'lkp-tests'))
 
         if lkp_commit:
-            self.results += utils.system_output('git checkout -B stable ' + commit, retain_output=True)
+            self.results += utils.system_output('git checkout -B stable ' + lkp_commit, retain_output=True)
         #
         # New distros use libarchive-tools and not bsdtar so edit the package name
         # https://github.com/intel/lkp-tests/issues/50
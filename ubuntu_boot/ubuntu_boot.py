0f0b71e0b42216b4f1e4ace9c839584556cd49d9
Po-Hsu Lin <po-hsu.lin@canonical.com>
2020-07-08 19:47:25 +0800

    UBUNTU: SAUCE: ubuntu_boot: add tests to check error in log and kernel_tainted flags
    Add two sub-tests into the boot test to help us catching issues like
    lp:1840046 ("BUG: non-zero pgtables_bytes on freeing mm: -16384") in the
    early stage:
      1. log_check test - use regex to search for error patterns in syslog
      2. kernel_tainted test - check for kernel tainted flags
    
    Call the original test as a boot_smoke_test, so now there will be 3
    tests under this ubuntu_boot test.
    
    This ubuntu_boot test will be executed when the kernel was copied to
    our ppa.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Sean Feole <sean.feole@canonical.com>
    Acked-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_boot/ubuntu_boot.py
+++ ubuntu_boot/ubuntu_boot.py
@@ -1,12 +1,62 @@
 import os
+import re
 from autotest.client import test, utils
+from autotest.client.shared import error
 
 
 class ubuntu_boot(test.test):
     version = 1
+    def log_check(self):
+        '''Test for checking error patterns in log files'''
+        # dmesg will be cleared out in autotest with dmesg -c before the test starts
+        # Let's check for /var/log/syslog instead
+        logfile = '/var/log/syslog'
+        patterns = [
+            'kernel: \[ *\d+\.\d+\] BUG:',
+            'kernel: \[ *\d+\.\d+\] Oops:',
+            'kernel: \[ *\d+\.\d+\] kernel BUG at',
+            'kernel: \[ *\d+\.\d+\] WARNING:'
+        ]
+        test_passed = True
+        print('Checking error message in {}:'.format(logfile))
+        if os.path.exists(logfile):
+            with open(logfile) as f:
+                content = f.read()
+                for pat in patterns:
+                    print('Scanning for pattern "{}"'.format(pat))
+                    if re.search(pat, content):
+                        print('Pattern found, Log NOT clean.')
+                        test_passed = False
+        else:
+            print('Log file was not found.')
+            test_passed = False
+        return test_passed
 
-    def run_once(self, test_time=10, exit_on_error=True, set_time=True):
+    def kernel_tainted(self):
+        '''Test for checking kernel tatined flags'''
+        test_passed = True
+        print('Checking kernel tainted flags in /proc/sys/kernel/tainted')
+        with open('/proc/sys/kernel/tainted') as f:
+            content = f.read()
+            if content != '0\n':
+                test_passed = False
+        return test_passed
+
+    def run_once(self, test_name, exit_on_error=True):
+        if test_name == 'log_check':
+            if not self.log_check():
+                raise error.TestFail()
+            else:
+                print("GOOD: Log clean.")
+            return
+        elif test_name == 'kernel_tainted':
+            if not self.kernel_tainted():
+                raise error.TestFail()
+            else:
+                print('GOOD: Kernel not tainted.')
+            return
+
         cmd = "uname -a"
         utils.system(cmd)
-        cmd = "lsb_release"
+        cmd = "lsb_release -a"
         utils.system(cmd)

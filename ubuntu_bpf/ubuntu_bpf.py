ColinIanKing/autotest-client-tests

commit 79fdddfe665028d2dbf1833e2c1e0c7355ea950e

UBUNTU: SAUCE: ubuntu_bpf: use clang-10 and llvm-10 for F-5.6
BugLink: https://bugs.launchpad.net/bugs/1879360

The ubuntu_bpf test can't be built with clang-9 on 5.6 kernel.
Use clang-10 and llvm-10 instead to solve this problem.

Also we still need to keep clang-9 for 5.4 kernel in Focal,
otherwise the test_map test will fail to execute on s390x with:
  libbpf: Error loading BTF: Invalid argument(22)

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_bpf/ubuntu_bpf.py
@@ -20,7 +20,11 @@ class ubuntu_bpf(test.test):
         pkgs.append(gcc)
 
         if self.series == 'focal':
-            pkgs.extend(['clang-9', 'llvm-9'])
+            if self.kv.startwith('5.6.0'):
+                # Specical case of F-oem-5.6 (lp:1879360)
+                pkgs.extend(['clang-10', 'llvm-10'])
+            else:
+                pkgs.extend(['clang-9', 'llvm-9'])
         else:
             pkgs.extend(['clang', 'llvm'])
 
@@ -56,10 +60,16 @@ class ubuntu_bpf(test.test):
         # llvm10 breaks bpf test_maps, revert to llvm9 instead
         #
         if self.series == 'focal':
-            os.environ["CLANG"] = "clang-9"
-            os.environ["LLC"] = "llc-9"
-            os.environ["LLVM_OBJCOPY"] = "llvm-objcopy-9"
-            os.environ["LLVM_READELF"] = "llvm-readelf-9"
+            if self.kv.startwith('5.6.0'):
+                os.environ["CLANG"] = "clang-10"
+                os.environ["LLC"] = "llc-10"
+                os.environ["LLVM_OBJCOPY"] = "llvm-objcopy-10"
+                os.environ["LLVM_READELF"] = "llvm-readelf-10"
+            else:
+                os.environ["CLANG"] = "clang-9"
+                os.environ["LLC"] = "llc-9"
+                os.environ["LLVM_OBJCOPY"] = "llvm-objcopy-9"
+                os.environ["LLVM_READELF"] = "llvm-readelf-9"
 
         utils.make('-C linux/tools/testing/selftests TARGETS=bpf clean all')
 

commit ff0ea618b1db31d77a0a6f43c186f928380b191c

UBUNTU: SAUCE: ubuntu_bpf: another startwith typo fix
Fix for another startswith typo.

Fixes: 79fdddfe6650 ("UBUNTU: SAUCE: ubuntu_bpf: use clang-10 and llvm-10 for F-5.6")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_bpf/ubuntu_bpf.py
@@ -66,7 +66,7 @@ class ubuntu_bpf(test.test):
         # llvm10 breaks bpf test_maps, revert to llvm9 instead
         #
         if self.series == 'focal':
-            if self.kv.startwith('5.6.0'):
+            if self.kv.startswith('5.6.0'):
                 os.environ["CLANG"] = "clang-10"
                 os.environ["LLC"] = "llc-10"
                 os.environ["LLVM_OBJCOPY"] = "llvm-objcopy-10"
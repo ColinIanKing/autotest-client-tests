ColinIanKing/autotest-client-tests

commit 77c2badedd4b7049918a80b14888fd1ec65f8b77

UBUNTU: SAUCE: python3 print() parentheses compatibility fix
Eventually we might need to switch to python3 in the future.
Add parentheses to print for python3.

Changes are like:
- print "string ", var
+ print("string " + var)

- print
+ print("")

- print "%s_bogoops" % (test_name), "%.3f " * len(values) % tuple(values)
+ print("%s_bogoops " % (test_name) + "%.3f " * len(values) % tuple(values))

- print sys.stderr, keylist
+ from __future__ import print_function
+ print(sys.stderr, keylist)

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -140,7 +140,7 @@ class ubuntu_kernel_selftests(test.test):
             # this test breaks 5.0+ (and maybe earlier), disable it for now
             # 5/12/2019, LP#1854968
             #
-            print "Disabling l2tp.sh"
+            print("Disabling l2tp.sh")
             fn = 'linux/tools/testing/selftests/net/l2tp.sh'
             if os.path.exists(fn):
                 cmd = 'chmod -x ' + fn
@@ -149,7 +149,7 @@ class ubuntu_kernel_selftests(test.test):
             #
             # update fix CPU hotplug test, new and old versions
             #
-            print "Updating CPU hotplug test"
+            print("Updating CPU hotplug test")
             fn="linux/tools/testing/selftests/cpu-hotplug/cpu-on-off-test.sh"
             if os.path.exists(fn) and 'present_cpus=' not in open(fn).read():
                 cmd = 'cp %s/cpu-on-off-test.sh %s' % (self.bindir, fn)
@@ -195,7 +195,7 @@ class ubuntu_kernel_selftests(test.test):
             fn = 'linux/tools/testing/selftests/ptrace/vmaccess.c'
             mk = 'linux/tools/testing/selftests/ptrace/Makefile'
             if os.path.exists(fn):
-                print "Disabling ptrace/vmacces"
+                print("Disabling ptrace/vmacces")
                 cmd = 'sed -i "s/ vmaccess//" ' + mk
                 utils.system(cmd)
 

commit 008f6a9bcb8b5591f724b2e0ac12c8346df28242

UBUNTU: SAUCE: fix mispalced format()
There are several format() were misplaced after print() instead of the
string. Correct this issue.

Fixes: 77c2badedd4b ("UBUNTU: SAUCE: python3 print() parentheses compatibility fix")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -73,7 +73,7 @@ class ubuntu_kernel_selftests(test.test):
         failures = list(re.finditer(pattern, self.results))
         if failures:
             for i in failures:
-                print('Sub test case: {} failed.').format(i.group('case'))
+                print('Sub test case: {} failed.'.format(i.group('case')))
             return True
         return False
 

ubuntu_ltp/ubuntu_ltp.py
@@ -134,7 +134,7 @@ class ubuntu_ltp(test.test):
 
         num_failed = sum(1 for line in open(log_failed))
         print("== Test Suite Summary ==")
-        print("{} test cases failed").format(num_failed)
+        print("{} test cases failed".format(num_failed))
 
         if num_failed > 0:
             cmd = "awk '{print$1}' " + log_failed + " | sort | uniq | tr '\n' ' '"

ubuntu_ltp_syscalls/ubuntu_ltp_syscalls.py
@@ -182,8 +182,8 @@ class ubuntu_ltp_syscalls(test.test):
         num_failed = sum(1 for line in open(log_failed))
         num_blacklisted = len(blacklisted) if blacklisted else 0
         print("== Test Suite Summary ==")
-        print("{} test cases failed").format(num_failed)
-        print("{} test cases blacklisted").format(num_blacklisted)
+        print("{} test cases failed".format(num_failed))
+        print("{} test cases blacklisted".format(num_blacklisted))
 
         if num_failed > 0:
             cmd = "awk '{print$1}' " + log_failed + " | sort | uniq | tr '\n' ' '"
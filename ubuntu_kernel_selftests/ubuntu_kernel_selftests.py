80166e50ea96ece2289b1794fac0dac91668888c
Seth Forshee <seth.forshee@canonical.com>
2020-12-10 07:48:15 -0600

    UBUNTU: SAUCE: ubuntu_kernel_selftests -- Fix python3-docutils package name
    "UBUNTU: SAUCE: ubuntu_kernel_selftests -- Install python3-docutils for
    5.10 and later" had a typo, fix it.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
+++ ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -47,7 +47,7 @@ class ubuntu_kernel_selftests(test.test):
 
         if self.kv >= 510:
             # python3-docutils is needed for bpf selftests build
-            pkgs.extend(['pyhon3-docutils'])
+            pkgs.extend(['python3-docutils'])
 
         cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
         self.results = utils.system_output(cmd, retain_output=True)

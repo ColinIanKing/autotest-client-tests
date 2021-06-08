ColinIanKing/autotest-client-tests

commit e14cb0e1bd0c71129ff1bcfd7c6c4e5f6200b50a

UBUNTU SAUCE: ubuntu_boot Centos Support
Some updates to add support for the a-c-t ubuntu_boot
test.

- Add logic to identify if SUT is ubuntu vs centos
  * Update the location of /var/log/messages if so.
- Reorganize tests order so kernel_tainted runs last.

Signed-off-by: sfeole <sean.feole@canonical.com>
Acked-by: Francis Ginther <francis.ginther@canonical.com>

ubuntu_boot/ubuntu_boot.py
@@ -1,16 +1,27 @@
 import os
 import re
+import platform
 from autotest.client import test, utils
 from autotest.client.shared import error
 
 
 class ubuntu_boot(test.test):
     version = 1
+    def setup(self):
+        '''Centos Specific Boot Test Checks'''
+        self.centos = False
+        os_dist = platform.linux_distribution()[0].split(' ')[0]
+        if os_dist == 'CentOS':
+            self.centos = True
+
     def log_check(self):
         '''Test for checking error patterns in log files'''
         # dmesg will be cleared out in autotest with dmesg -c before the test starts
         # Let's check for /var/log/syslog instead
-        logfile = '/var/log/syslog'
+        if self.centos:
+            logfile = '/var/log/messages'
+        else:
+            logfile = '/var/log/syslog'
         patterns = [
             'kernel: \[ *\d+\.\d+\] BUG:.*',
             'kernel: \[ *\d+\.\d+\] Oops:.*',

ubuntu_boot/control.ubuntu
@@ -8,6 +8,6 @@ TEST_CATEGORY = 'Smoke'
 DOC = '''
 '''
 
-job.run_test_detail('ubuntu_boot', test_name='kernel_tainted', tag='kernel_tainted', timeout=60*5)
 job.run_test_detail('ubuntu_boot', test_name='log_check', tag='log_check', timeout=60*5)
 job.run_test_detail('ubuntu_boot', test_name='boot_smoke_test', tag='boot_smoke_test', timeout=60*5)
+job.run_test_detail('ubuntu_boot', test_name='kernel_tainted', tag='kernel_tainted', timeout=60*5)

commit afc27d69401741613204c054bd699b98f22210f5

UBUNTU: SAUCE: ubuntu_boot: fix second test execution
The setup() might not be called on second test run and actually autotest
design assumes setup() for given version will be called exactly once.
It is expected from setup() to modify only persistent state of the
system, so on next run it's execution is not needed.  Runtime-only
settings should not be configured via setup().

This fixes failure on second test execution:

    00:54:03 INFO | 	START	ubuntu_boot.log_check	ubuntu_boot.log_check
    00:54:03 DEBUG| Persistent state client._record_indent now set to 2
    00:54:03 DEBUG| Persistent state client.unexpected_reboot now set to ('ubuntu_boot.log_check', 'ubuntu_boot.log_check')
    00:54:03 DEBUG| Waiting for pid 19403 for 300 seconds
    00:54:03 WARNI| System python is too old, crash handling disabled
    00:54:03 ERROR| Exception escaping from test:
    Traceback (most recent call last):
      File "/home/azure/autotest/client/shared/test.py", line 411, in _exec
        _call_test_function(self.execute, *p_args, **p_dargs)
      File "/home/azure/autotest/client/shared/test.py", line 830, in _call_test_function
        raise error.UnhandledTestFail(e)
    UnhandledTestFail: Unhandled AttributeError: 'ubuntu_boot' object has no attribute 'centos'
    Traceback (most recent call last):
      File "/home/azure/autotest/client/shared/test.py", line 823, in _call_test_function
        return func(*args, **dargs)
      File "/home/azure/autotest/client/shared/test.py", line 291, in execute
        postprocess_profiled_run, args, dargs)
      File "/home/azure/autotest/client/shared/test.py", line 212, in _call_run_once
        self.run_once(*args, **dargs)
      File "/home/azure/autotest/client/tests/ubuntu_boot/ubuntu_boot.py", line 63, in run_once
        if not self.log_check():
      File "/home/azure/autotest/client/tests/ubuntu_boot/ubuntu_boot.py", line 25, in log_check
        if self.centos:
    AttributeError: 'ubuntu_boot' object has no attribute 'centos'

Fixes: e14cb0e1bd0c ("UBUNTU SAUCE: ubuntu_boot Centos Support")

ubuntu_boot/ubuntu_boot.py
@@ -12,17 +12,14 @@ class ubuntu_boot(test.test):
         cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
         self.results = utils.system_output(cmd, retain_output=True)
 
-        '''Centos Specific Boot Test Checks'''
-        self.centos = False
-        os_dist = platform.linux_distribution()[0].split(' ')[0]
-        if os_dist == 'CentOS':
-            self.centos = True
-
     def log_check(self):
         '''Test for checking error patterns in log files'''
+        '''Centos Specific Boot Test Checks'''
+        os_dist = platform.linux_distribution()[0].split(' ')[0]
+
         # dmesg will be cleared out in autotest with dmesg -c before the test starts
         # Let's check for /var/log/syslog instead
-        if self.centos:
+        if os_dist == 'CentOS':
             logfile = '/var/log/messages'
         else:
             logfile = '/var/log/syslog'
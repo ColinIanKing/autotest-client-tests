ColinIanKing/autotest-client-tests

commit 0634a0e49f40ee72609ef991c244e21e47cd7172

UBUNTU: SAUCE: ubuntu_kernel_selftests: remove workaround for ftrace/kprobe_args_user.tc
This kprobe_args_user.tc in ftrace was disabled due to lp:1869227 it
will be marked as unresolved if not supported and marked as a failure.

With patch b730d668138cb3 ("ftrace/selftest: make unresolved cases
cause failure if --fail-unresolved set") landed (lp:1877958), an
unresolved case will not be treated as a failure unless we requested.
Therefore we can remove the workaround here.

Also note that with patch 1e11b7dbef17bb ("selftests/ftrace: Return
unsupported for the unconfigured features") this test will now be
marked correctly as unsupported.

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -173,19 +173,6 @@ class ubuntu_kernel_selftests(test.test):
             ]
 
             #
-            # Ftrace 'Kprobe event user-memory access' test depends on
-            # HAVE_FUNCTION_ARG_ACCESS_API, but ppc64 doesn't support it:
-            # disable it to avoid an unresolved result (and thus a failure).
-            #
-            if self.arch in ['ppc64le', 's390x']:
-                filenames.append('kprobe/kprobe_args_user.tc')
-
-            for fn in filenames:
-                fn = 'linux/tools/testing/selftests/ftrace/test.d/' + fn
-                if os.path.exists(fn):
-                    os.remove(fn)
-
-            #
             # ptrace/vmaccess was introduced in 5.7-rc1 and is broken ATM,
             # see https://lkml.org/lkml/2020/4/9/648
             #

commit 16110926711b60765d9798d98536332beb291e05

"UBUNTU: SAUCE: ubuntu_kernel_selftests: restore accidentally removed testcase removal code"
While removing the kprobe_args_user.tc blacklist in ftrace in commit
0634a0e49f40ee ("UBUNTU: SAUCE: ubuntu_kernel_selftests: remove
workaround for ftrace/kprobe_args_user.tc"). The testcase removal code
was accidentally deleted.

We are seeing failures on those test cases that should be removed,
restore this code snippet as we don't want them to run at the very
beginning.

Fixes: 0634a0e49f40ee ("UBUNTU: SAUCE: ubuntu_kernel_selftests: remove
workaround for ftrace/kprobe_args_user.tc")
Reported-by: Francis Ginther <francis.ginther@canonical.com>
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

ubuntu_kernel_selftests/ubuntu_kernel_selftests.py
@@ -170,6 +170,11 @@ class ubuntu_kernel_selftests(test.test):
                 'kprobe/kprobe_module.tc',
             ]
 
+            for fn in filenames:
+                fn = 'linux/tools/testing/selftests/ftrace/test.d/' + fn
+                if os.path.exists(fn):
+                    os.remove(fn)
+
             #
             # ptrace/vmaccess was introduced in 5.7-rc1 and is broken ATM,
             # see https://lkml.org/lkml/2020/4/9/648
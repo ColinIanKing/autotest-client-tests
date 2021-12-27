128adf0c28a6ca117641148abf8bdaf9a5c3b83e
Po-Hsu Lin <po-hsu.lin@canonical.com>
2021-12-27 17:12:11 +0800

    UBUNTU: SAUCE: ubuntu_ltp_kernel_misc: install modules-extra package for zram tests
    BugLink: https://bugs.launchpad.net/bugs/1955793
    
    The modules-extra package is required for zram01 / zram02 / zram03
    tests in ubuntu_ltp_kernel_misc.
    
    We used to install this package before splitting kernel_misc test
    out from ubuntu_ltp.
    
    Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

--- ubuntu_ltp_kernel_misc/ubuntu_ltp_kernel_misc.py
+++ ubuntu_ltp_kernel_misc/ubuntu_ltp_kernel_misc.py
@@ -50,6 +50,9 @@ class ubuntu_ltp_kernel_misc(test.test):
         gcc = 'gcc' if arch in ['ppc64le', 'aarch64', 's390x', 'riscv64'] else 'gcc-multilib'
         pkgs.append(gcc)
 
+        if any(x in self.flavour for x in ['aws', 'azure', 'gcp', 'gke']):
+            pkgs.append('linux-modules-extra-' + platform.uname()[2])
+
         cmd = 'yes "" | DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes ' + ' '.join(pkgs)
         self.results = utils.system_output(cmd, retain_output=True)
 
